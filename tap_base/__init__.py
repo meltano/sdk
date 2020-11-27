fc826e4262f48db4ed31f4402ef07d759e02beec
AJ Steers <aj.steers@slalom.com>
2020-11-26 21:52:39 -0800

    populate __init__

--- tap_base/__init__.py
+++ tap_base/__init__.py
@@ -1,1 +1,7 @@
+"""tap-base library for building singer-compliant taps"""
 
+from tap_base.PluginBase import PluginBase
+from tap_base.TapBase import TapBase
+from tap_base.TapStreamBase import TapStreamBase
+
+__all__ = ["PluginBase", "TapBase", "TapStreamBase"]
