7ecfeaa213023168cb7c9c0f93131ae2aa2a72e4
AJ Steers <aj.steers@slalom.com>
2020-12-08 23:22:38 -0800

    add end to end test

--- tap_base/tests/sample_target_parquet/tests/test_end_to_end.py
+++ tap_base/tests/sample_target_parquet/tests/test_end_to_end.py
@@ -1,13 +1,22 @@
 """Test tap-to-target sync."""
 
+import io
+from pathlib import Path
+from contextlib import redirect_stdout
+from datetime import datetime
+
 from tap_base.tests.sample_tap_parquet.tap import SampleTapParquet
 from tap_base.tests.sample_target_parquet.target import SampleTargetParquet
 
-TAP_CONFIG = {}
+SAMPLE_FILENAME = "/tmp/testfile.parquet"
+SAMPLE_TAP_CONFIG = {"filepath": SAMPLE_FILENAME}
+SAMPLE_TARGET_CONFIG = {"filepath": f"{SAMPLE_FILENAME}-{datetime.now()}"}
 
 
 def test_tap_to_target():
-    tap = SampleTapParquet(config=TAP_CONFIG, state=None)
-    target = SampleTargetParquet(config=TAP_CONFIG, state=None)
-    tap_output: str = tap.sync_all(allow_load=False, allow_discover=True)
-    target.process_lines(tap_output.splitlines())
+    tap = SampleTapParquet(config=SAMPLE_TAP_CONFIG, state=None)
+    target = SampleTargetParquet(config=SAMPLE_TARGET_CONFIG, state=None)
+    with io.StringIO() as buf, redirect_stdout(buf):
+        tap.sync_all(allow_load=False, allow_discover=True)
+        target.process_lines(buf)
+    assert Path(SAMPLE_TARGET_CONFIG["filepath"]).exists()
