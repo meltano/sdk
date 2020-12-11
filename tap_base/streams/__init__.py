823fab29a8ff51f265ac9894b3e8295c775d0963
AJ Steers <aj.steers@slalom.com>
2020-12-10 20:47:49 -0800

    fix `streams` init

--- tap_base/streams/__init__.py
+++ tap_base/streams/__init__.py
@@ -1,15 +1,12 @@
 """tap-base library for building singer-compliant taps."""
 
-# from tap_base.streams.generic import GenericStreamBase
-# from tap_base.streams.syncable import TapStreamBase
-
-# from tap_base.streams.database import DiscoverableStreamBase, DatabaseStreamBase
+from tap_base.streams.core import TapStreamBase
+from tap_base.streams.api import APIStreamBase
 from tap_base.streams.database import DatabaseStreamBase
 
 
 __all__ = [
-    # "GenericStreamBase",
-    # "TapStreamBase",
-    # "DiscoverableStreamBase",
+    "TapStreamBase",
+    "APIStreamBase",
     "DatabaseStreamBase",
 ]
