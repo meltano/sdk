33a2bdf070b78ebf05150436c74f121821cc139a
AJ Steers <aj.steers@slalom.com>
2020-12-17 22:31:58 -0800

    gitlab streams simplification

--- tap_base/tests/sample_tap_gitlab/gitlab_rest_streams.py
+++ tap_base/tests/sample_tap_gitlab/gitlab_rest_streams.py
@@ -1,17 +1,18 @@
 """Sample tap stream test for tap-gitlab."""
 
-from datetime import datetime
-from typing import Any, Dict, Union
+from pathlib import Path
+from typing import Any, Dict
 
 from tap_base.streams.rest import RESTStreamBase, URLArgMap
 
-SITE_URL = "https://gitlab.com/api/v4"
+SCHEMAS_DIR = Path("./tap_base/tests/sample_tap_gitlab/schemas")
 
 
 class GitlabStream(RESTStreamBase):
     """Sample tap test for gitlab."""
 
-    site_url_base = SITE_URL
+    tap_name = "sample-tap-gitlab"
+    site_url_base = "https://gitlab.com/api/v4"
 
     def get_auth_header(self) -> Dict[str, Any]:
         """Return an authorization header for REST API requests."""
@@ -25,6 +26,8 @@ class GitlabStream(RESTStreamBase):
         return row
 
     def get_url(self, url_suffix: str = None, extra_url_args: URLArgMap = None) -> str:
+        # TODO: Handle this automatically in the framework's base class,
+        #       apply template logic to fill placeholders.
         replacement_map = {
             # TODO: Handle multiple projects:
             "project_id": self.get_config("project_ids")[0],
@@ -33,3 +36,37 @@ class GitlabStream(RESTStreamBase):
         if extra_url_args:
             replacement_map.update(extra_url_args)
         return super().get_url(url_suffix=url_suffix, extra_url_args=replacement_map)
+
+
+class ProjectsStream(GitlabStream):
+    stream_name = "projects"
+    url_suffix = "/projects/{project_id}?statistics=1"
+    primary_keys = ["id"]
+    replication_key = None
+    schema_filepath = SCHEMAS_DIR / "projects.json"
+
+
+class ReleasesStream(GitlabStream):
+    stream_name = "releases"
+    url_suffix = "/projects/{project_id}/releases"
+    primary_keys = ["project_id", "commit_id", "tag_name"]
+    replication_key = None
+    schema_filepath = SCHEMAS_DIR / "releases.json"
+
+
+class IssuesStream(GitlabStream):
+    stream_name = "issues"
+    url_suffix = "/projects/{project_id}/issues?scope=all&updated_after={start_date}"
+    primary_keys = ["id"]
+    replication_key = None
+    schema_filepath = SCHEMAS_DIR / "issues.json"
+
+
+class CommitsStream(GitlabStream):
+    stream_name = "commits"
+    url_suffix = (
+        "/projects/{project_id}/repository/commits?since={start_date}&with_stats=true"
+    )
+    primary_keys = ["id"]
+    replication_key = None
+    schema_filepath = SCHEMAS_DIR / "commits.json"
--- tap_base/tests/sample_tap_gitlab/gitlab_tap.py
+++ tap_base/tests/sample_tap_gitlab/gitlab_tap.py
@@ -1,85 +1,45 @@
 """Sample tap test for tap-gitlab."""
 
 import json
-from logging import Logger
-from tap_base.streams.rest import RESTStreamBase
-from typing import Any, Dict, List, Optional, Type, Union
 from pathlib import Path
-
-from singer.schema import Schema
 
 import click
 
-from tap_base.helpers import classproperty
 from tap_base.tap_base import TapBase
-from tap_base.tests.sample_tap_gitlab.gitlab_rest_streams import GitlabStream
+from tap_base.tests.sample_tap_gitlab.gitlab_rest_streams import (
+    GitlabStream,
+    ProjectsStream,
+    ReleasesStream,
+    IssuesStream,
+    CommitsStream,
+)
 from tap_base.tests.sample_tap_gitlab.gitlab_graphql_streams import (
     GraphQLCurrentUserStream,
 )
 
-
-ACCEPTED_CONFIG_OPTIONS = ["auth_token", "project_ids", "start_date"]
-REQUIRED_CONFIG_SETS = [["auth_token", "project_ids", "start_date"]]
-SCHEMAS_DIR = "./tap_base/tests/sample_tap_gitlab/schemas"
-
-STREAM_TYPES: Dict[
-    Type[RESTStreamBase], Dict[str, Dict[str, Union[None, str, List[str]]]],
-] = {
-    GitlabStream: {
-        "projects": {
-            "url_suffix": "/projects/{project_id}?statistics=1",
-            "primary_keys": ["id"],
-            "replication_key": None,
-        },
-        "releases": {
-            "url_suffix": "/projects/{project_id}/releases",
-            "primary_keys": ["project_id", "commit_id", "tag_name"],
-            "replication_key": None,
-        },
-        "issues": {
-            "url_suffix": "/projects/{project_id}/issues?scope=all&updated_after={start_date}}",
-            "primary_keys": ["id"],
-            "replication_key": None,
-        },
-        "commits": {
-            "url_suffix": "/projects/{project_id}/repository/commits?since={start_date}&with_stats=true",
-            "primary_keys": ["id"],
-            "replication_key": None,
-        },
-    },
-}
+STREAM_TYPES = [
+    GitlabStream,
+    ProjectsStream,
+    ReleasesStream,
+    IssuesStream,
+    CommitsStream,
+    GraphQLCurrentUserStream,
+]
 
 
 class SampleTapGitlab(TapBase):
     """Sample tap for Gitlab."""
 
     name: str = "sample-tap-gitlab"
-    accepted_config_keys = ACCEPTED_CONFIG_OPTIONS
-    required_config_options = REQUIRED_CONFIG_SETS
+    accepted_config_keys = ["auth_token", "project_ids", "start_date"]
+    required_config_options = [["auth_token", "project_ids", "start_date"]]
 
     def discover_catalog_streams(self) -> None:
         """Initialize self._streams with a dictionary of all streams."""
-        # Add REST Streams
-        for stream_class, streams_dict in STREAM_TYPES.items():
-            for stream_name, stream_def in streams_dict.items():
-                schemafile = Path(SCHEMAS_DIR) / f"{stream_name}.json"
-                schema = json.loads(Path(schemafile).read_text())
-                new_stream = stream_class(
-                    name=stream_name,
-                    schema=schema,
-                    state={},
-                    config=self._config,
-                    url_pattern=stream_def["url_suffix"],
-                )
-                new_stream.primary_keys = stream_def.get("primary_keys", None)
-                new_stream.replication_key = stream_def.get("replication_key", None)
-                self._streams[stream_name] = new_stream
-
-        # Add GraphQL Streams
-        new_stream = GraphQLCurrentUserStream(
-            config=self._config, state={}
-        )
-        self._streams[new_stream.name] = new_stream
+        # Add REST and GraphQL Streams
+        for stream_class in STREAM_TYPES:
+            stream = stream_class(config=self._config, state={})
+            self._streams[stream.stream_name] = stream
 
 
 # CLI Execution:
