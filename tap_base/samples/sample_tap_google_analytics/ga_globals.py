6a73313218b01e50d5c42a87b03f3e27b9b20034
AJ Steers <aj.steers@slalom.com>
2020-12-23 15:03:06 -0800

    specify required fields

--- tap_base/samples/sample_tap_google_analytics/ga_globals.py
+++ tap_base/samples/sample_tap_google_analytics/ga_globals.py
@@ -1,5 +1,9 @@
 """Global settings and helper functions."""
 
 PLUGIN_NAME = "sample-tap-google-analytics"
-ACCEPTED_CONFIG_OPTIONS = ["client_id", "client_secret"]
-REQUIRED_CONFIG_SETS = None
+ACCEPTED_CONFIG_OPTIONS = [
+    "view_id",
+    "client_email",
+    "private_key",
+]
+REQUIRED_CONFIG_SETS = [["view_id", "client_email", "private_key",]]
