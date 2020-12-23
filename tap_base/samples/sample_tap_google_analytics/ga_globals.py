962ffbbd2419f555d5e4a2791155c47548cfdc35
AJ Steers <aj.steers@slalom.com>
2020-12-23 14:54:50 -0800

    use globals.p file

--- tap_base/samples/sample_tap_google_analytics/ga_globals.py
+++ tap_base/samples/sample_tap_google_analytics/ga_globals.py
@@ -1,3 +1,5 @@
 """Global settings and helper functions."""
 
 PLUGIN_NAME = "sample-tap-google-analytics"
+ACCEPTED_CONFIG_OPTIONS = ["client_id", "client_secret"]
+REQUIRED_CONFIG_SETS = None
--- tap_base/samples/sample_tap_google_analytics/ga_tap.py
+++ tap_base/samples/sample_tap_google_analytics/ga_tap.py
@@ -4,25 +4,25 @@ import json
 from pathlib import Path
 from typing import List
 
-from singer.schema import Schema
-
 from tap_base.tap_base import TapBase
 from tap_base.samples.sample_tap_google_analytics.ga_tap_stream import (
     GASimpleSampleStream,
     SampleGoogleAnalyticsStream,
 )
+from tap_base.samples.sample_tap_google_analytics.ga_globals import (
+    PLUGIN_NAME,
+    ACCEPTED_CONFIG_OPTIONS,
+    REQUIRED_CONFIG_SETS,
+)
 
-ACCEPTED_CONFIG_OPTIONS = ["client_id", "client_secret"]
 REPORT_DEFS_FILE = "tap_base/samples/sample_tap_google_analytics/resources/default_report_definitions.json"
-REQUIRED_CONFIG_SETS = None
-
 REPORT_DEFS = json.loads(Path(REPORT_DEFS_FILE).read_text())
 
 
 class SampleTapGoogleAnalytics(TapBase):
     """Sample tap for GoogleAnalytics."""
 
-    name: str = "sample-tap-google_analytics"
+    name: str = PLUGIN_NAME
     accepted_config_keys = ACCEPTED_CONFIG_OPTIONS
     required_config_options = REQUIRED_CONFIG_SETS
     default_stream_class = SampleGoogleAnalyticsStream
