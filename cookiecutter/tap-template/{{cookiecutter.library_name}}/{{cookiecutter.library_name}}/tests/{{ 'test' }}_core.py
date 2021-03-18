56f1c12c2ec0f24c79d7955c6a3729bdaefb7b63
AJ Steers <aaronsteers@gmail.com>
2021-03-18 12:57:55 -0700

    fix parquet refs

--- cookiecutter/tap-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/tests/{{ 'test' }}_core.py
+++ cookiecutter/tap-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/tests/{{ 'test' }}_core.py
@@ -1,7 +1,8 @@
 """Tests init and discovery features for {{ cookiecutter.tap_id }}."""
 
-from singer_sdk.samples.sample_tap_parquet.parquet_tap import SampleTapParquet
 from singer_sdk.helpers.util import utc_now
+
+from {{ cookiecutter.library_name }}.tap import Tap{{ cookiecutter.source_name }}
 
 SAMPLE_CONFIG = {
     "start_date": utc_now()
@@ -13,6 +14,8 @@ SAMPLE_CONFIG = {
 
 def test_catalog_discovery():
     """Test stream catalog discovery."""
-    tap = SampleTapParquet(config=SAMPLE_CONFIG, state=None, parse_env_config=True)
+    tap = Tap{{ cookiecutter.source_name }}(
+        config=SAMPLE_CONFIG, state=None, parse_env_config=True
+    )
     catalog_json = tap.run_discovery()
     assert catalog_json
