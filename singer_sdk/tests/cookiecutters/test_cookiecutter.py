39b1c95a1c2f7d33160ea489cf54f330d44e4f8c
AJ Steers <aaronsteers@gmail.com>
2021-03-17 21:16:20 -0700

    add cookiecutters in pytest tests

---
+++ singer_sdk/tests/cookiecutters/test_cookiecutter.py
@@ -0,0 +1,22 @@
+"""Test cookiecutter template."""
+
+import yaml
+from cookiecutter.main import cookiecutter
+
+from pathlib import Path
+
+COOKIECUTTER_DIR = "../cookiecutter/tap-template"
+TESTS_INPUT_FILE = "../../cookiecutter/tap-template/cookiecutter.tests.yml"
+
+
+def test_cookiecutter_templates():
+    """Test sync_all() for countries sample."""
+    template_inputs = yaml.loads(Path(TESTS_INPUT_FILE).read_text())["tests"]
+    for input in template_inputs:
+        cookiecutter(
+            template=COOKIECUTTER_DIR,
+            output_dir=f".output/{input['tap_id']}",
+            extra_context=input,
+            overwrite_if_exists=True,
+            no_input=True,
+        )
