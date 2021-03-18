94032ead72b8fa32ea7a2ba6b2ebd1965e5f86e8
AJ Steers <aaronsteers@gmail.com>
2021-03-18 13:11:15 -0700

    add mypy cookiecutter test

--- singer_sdk/tests/cookiecutters/test_cookiecutter.py
+++ singer_sdk/tests/cookiecutters/test_cookiecutter.py
@@ -1,12 +1,13 @@
 """Test cookiecutter template."""
 
+import logging
 from pathlib import Path
 
 import black
-from flake8.api import legacy as flake8
 from cookiecutter.main import cookiecutter
+from flake8.api import legacy as flake8
+from mypy import api
 import yaml
-
 
 COOKIECUTTER_DIR = "cookiecutter/tap-template"
 TESTS_INPUT_FILE = "cookiecutter/tap-template/cookiecutter.tests.yml"
@@ -20,7 +21,7 @@ def test_cookiecutter_templates():
     )
     style_guide_strict = flake8.get_style_guide(
         ignore=[
-            "F401",  # imported but unused
+            "F401",  # "imported but unused"
         ]
     )
     for input in template_inputs:
@@ -32,7 +33,8 @@ def test_cookiecutter_templates():
             overwrite_if_exists=True,
             no_input=True,
         )
-        for outfile in Path(outdir).glob("**/*.py"):
+        outputfiles = list(Path(outdir).glob("**/*.py"))
+        for outfile in outputfiles:
             filepath = str(outfile.absolute())
             report = style_guide_easy.check_files([filepath])
             errors = report.get_statistics("E")
@@ -45,6 +47,11 @@ def test_cookiecutter_templates():
                 mode=black.FileMode(),
                 write_back=black.WriteBack.YES,
             )
+            mypy_out = api.run([filepath])
+            mypy_msg = str(mypy_out[0])
+            if not mypy_msg.startswith("Success:"):
+                logging.exception(f"My validation failed: {mypy_msg}")
+                assert not mypy_msg, f"MyPy validation failed for file {filepath}"
             report = style_guide_strict.check_files([filepath])
             errors = report.get_statistics("E")
             assert (
