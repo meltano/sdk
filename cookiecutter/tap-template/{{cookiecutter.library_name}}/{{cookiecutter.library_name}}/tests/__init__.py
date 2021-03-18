f3e6b9d91bcb7f90e07624076af820ddd6f34f79
AJ Steers <aaronsteers@gmail.com>
2021-03-17 19:22:51 -0700

    add a sample test in cookiecutter

---
+++ cookiecutter/tap-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Test suite for {{ cookicutter.library_name }}."""
---
+++ cookiecutter/tap-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/tests/test_core.py
@@ -0,0 +1,18 @@
+"""Tests init and discovery features for {{}}."""
+
+from singer_sdk.samples.sample_tap_parquet.parquet_tap import SampleTapParquet
+from singer_sdk.helpers.util import utc_now
+
+SAMPLE_CONFIG = {
+    "start_date": utc_now()
+    # TODO: Initialize minimal tap config and/or register env vars in test harness
+}
+
+# TODO: Expand tests as appropriate for your tap.
+
+
+def test_catalog_discovery():
+    """Test stream catalog discovery."""
+    tap = SampleTapParquet(config=SAMPLE_CONFIG, state=None, parse_env_config=True)
+    catalog_json = tap.run_discovery()
+    assert catalog_json
