b8b0971715dc0d2c3af5ce706eefa0175dea2437
AJ Steers <aaronsteers@gmail.com>
2021-03-22 15:55:54 -0700

    fix parquet sample file path

--- singer_sdk/tests/core/test_generic_tests.py
+++ singer_sdk/tests/core/test_generic_tests.py
@@ -1,11 +1,14 @@
 """Test the generic tests from `singer_sdk.helpers.testing`."""
 
+from pathlib import Path
+
 from singer_sdk.helpers.testing import get_basic_tap_test
 from singer_sdk.samples.sample_tap_countries.countries_tap import SampleTapCountries
 from singer_sdk.samples.sample_tap_parquet.parquet_tap import SampleTapParquet
 
 
-PARQUET_TEST_CONFIG = {"filepath": "/tmp/testfile.parquet"}
+PARQUET_SAMPLE_FILENAME = Path(__file__).parent / Path("./resources/testfile.parquet")
+PARQUET_TEST_CONFIG = {"filepath": str(PARQUET_SAMPLE_FILENAME)}
 
 
 test_tap_countries_basic_test = get_basic_tap_test(SampleTapCountries)
