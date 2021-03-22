72c8c396ef330e5e411fe2bbdd06fa3d08ef3428
AJ Steers <aaronsteers@gmail.com>
2021-03-22 15:16:49 -0700

    get ci external tap config from env vars

--- singer_sdk/helpers/testing.py
+++ singer_sdk/helpers/testing.py
@@ -6,7 +6,7 @@ from typing import Type, Callable
 
 def get_basic_tap_test(tap_class: Type[Tap], tap_config=None) -> Callable:
     def _test_fn(config=None):
-        tap = tap_class(config=config or tap_config)
+        tap = tap_class(config=config or tap_config, parse_env_config=True)
         tap.run_discovery()
         tap.run_connection_test()
 
