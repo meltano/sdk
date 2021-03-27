2f5d4926a5668804b628570a98a2612d9ef36326
AJ Steers <aaronsteers@gmail.com>
2021-03-26 18:44:47 -0700

    improve comments

--- singer_sdk/helpers/testing.py
+++ singer_sdk/helpers/testing.py
@@ -10,16 +10,19 @@ def get_basic_tap_test(tap_class: Type[Tap], tap_config=None) -> Callable:
     def _test_fn(config=None):
         # Initialize with basic config
         tap1: Tap = tap_class(config=config or tap_config, parse_env_config=True)
+        # Test CLI prints
         tap1.print_version()
         tap1.print_about()
         tap1.print_about(format="json")
+        # Test discovery
         tap1.run_discovery()
         catalog1 = tap1.catalog_dict
+        # Reset and re-initialize with an input catalog
         tap1 = None
-        # Initialize with an input catalog
         tap2: Tap = tap_class(
             config=config or tap_config, parse_env_config=True, catalog=catalog1
         )
+        # Run data sync test on all streams
         tap2.run_connection_test()
 
     return _test_fn
