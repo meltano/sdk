54e9f11f2efb6777bfe5c602e741d3959f55553f
AJ Steers <aaronsteers@gmail.com>
2021-03-30 00:15:50 -0700

    privatized env_var_config and is_secret_config

--- singer_sdk/tests/core/test_plugin_base.py
+++ singer_sdk/tests/core/test_plugin_base.py
@@ -25,13 +25,13 @@ def test_get_env_var_config():
             "PLUGIN_TEST_PROP3": "not-a-tap-setting",
         },
     ):
-        env_config = PluginTest.get_env_var_config()
+        env_config = PluginTest._env_var_config
         assert env_config["prop1"] == "hello"
         assert "PROP1" not in env_config
         assert "prop2" not in env_config and "PROP2" not in env_config
         assert "prop3" not in env_config and "PROP3" not in env_config
 
-    no_env_config = PluginTest.get_env_var_config()
+    no_env_config = PluginTest._env_var_config
     assert "prop1" not in no_env_config and "PROP1" not in env_config
     assert "prop2" not in no_env_config and "PROP2" not in env_config
     assert "prop3" not in no_env_config and "PROP3" not in env_config
