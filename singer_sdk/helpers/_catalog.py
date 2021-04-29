8366fdead5614bc23292e9c6ca592bccc263310e
AJ Steers <aaronsteers@gmail.com>
2021-04-29 13:12:20 -0700

    selection logic for streams and schema

--- singer_sdk/helpers/_catalog.py
+++ singer_sdk/helpers/_catalog.py
@@ -1,45 +1,126 @@
 """Private helper functions for catalog and selection logic."""
 
 from copy import deepcopy
-from typing import Optional, List
+from typing import Optional, List, Tuple, cast
+from logging import Logger
+
+from singer import metadata
+from singer.catalog import Catalog
+
 from singer_sdk.helpers._typing import is_object_type
 
 
-def get_selected_schema(schema: dict, catalog_entry: dict) -> dict:
+def is_stream_selected(
+    catalog: Optional[dict],
+    stream_name: str,
+    logger: Logger,
+):
+    """Return True if the stream is selected for extract."""
+    return is_property_selected(catalog, stream_name, breadcrumb=[], logger=logger)
+
+
+def is_property_selected(
+    catalog: Optional[dict],
+    stream_name: str,
+    breadcrumb: Optional[Tuple[str, ...]],
+    logger: Logger,
+):
+    """Return True if the property is selected for extract.
+
+    Breadcrumb of `[]` or `None` indicates the stream itself. Otherwise, the
+    breadcrumb is the path to a property within the stream.
+    """
+    if breadcrumb is None:
+        breadcrumb = cast(Tuple[str], ())
+    if isinstance(breadcrumb, str):
+        breadcrumb = tuple([breadcrumb])
+    if not isinstance(breadcrumb, tuple):
+        raise ValueError(
+            f"Expected tuple value for breadcrumb '{breadcrumb}'. "
+            f"Got {type(breadcrumb).__name__}"
+        )
+    if not catalog:
+        return True
+
+    catalog_obj = Catalog.from_dict(catalog)
+    catalog_entry = catalog_obj.get_stream(stream_name)
+    if not catalog_entry:
+        logger.warning(f"Catalog entry missing for '{stream_name}'. Skipping.")
+        return False
+
+    if not catalog_entry.metadata:
+        return True
+
+    md_map = metadata.to_map(catalog_entry.metadata)
+    md_entry = md_map.get(breadcrumb)
+    parent_value = None
+    if len(breadcrumb) > 0:
+        parent_value = is_property_selected(
+            catalog, stream_name, cast(Tuple[str], tuple(list(breadcrumb)[:-1])), logger
+        )
+    if parent_value is False:
+        return parent_value
+
+    if not md_entry:
+        logger.warning(
+            f"Catalog entry missing for '{stream_name}':'{breadcrumb}'. "
+            f"Using parent value of selected={parent_value}."
+        )
+        return parent_value
+
+    if md_entry.get("inclusion") == "unsupported":
+        return False
+
+    if md_entry.get("inclusion") == "automatic":
+        if md_entry.get("selected") is False:
+            logger.warning(
+                f"Property '{':'.join(breadcrumb)}' was deselected while also set"
+                "for automatic inclusion. Ignoring selected==False input."
+            )
+        return True
+
+    if "selected" in md_entry:
+        return md_entry["selected"]
+
+    raise ValueError(
+        f"Could not detect selection status for '{stream_name}' stream property "
+        f"'{':'.join(breadcrumb)}'"
+    )
+
+
+def get_selected_schema(catalog: dict, stream_name: str, logger: Logger) -> dict:
     """Return a copy of the provided JSON schema, dropping any fields not selected."""
-    result = deepcopy(schema)  # we don't want to modify the original
-    _pop_deselected_schema(result, catalog_entry, [])
-    return result
+    catalog_obj = Catalog.from_dict(catalog)
+    catalog_entry = catalog_obj.get_stream(stream_name)
+    schema = deepcopy(catalog_entry.schema.to_dict())
+    _pop_deselected_schema(schema, catalog, stream_name, cast(Tuple[str], ()), logger)
+    return schema
 
 
 def _pop_deselected_schema(
-    schema: dict, catalog_entry: dict, breadcrumb: List[str]
+    schema: dict,
+    catalog: dict,
+    stream_name: str,
+    breadcrumb: Tuple[str, ...],
+    logger: Logger,
 ) -> None:
-    """Pop anything from schema that is not selected.
+    """Remove anything schema that is not selected.
 
     Walk through schema, starting at the index in breadcrumb, recursively update in
     place.
     """
-    for prop, val in schema.items():
-        if not schema.get("selected", True) in [True, "selected"]:
-            schema.pop(prop)
+    for property_name, val in list(schema.get("properties", {}).items()):
+        logger.info(f"Checking '{property_name}' under '{breadcrumb}...")
+        property_breadcrumb: Tuple[str, ...] = tuple(list(breadcrumb) + [property_name])
+        selected = is_property_selected(
+            catalog, stream_name, property_breadcrumb, logger
+        )
+        if not selected:
+            schema["properties"].pop(property_name)
             continue
-        md = _get_metadata_by_breadcrumb(catalog_entry, breadcrumb + [prop])
-        if md and not md.get("selected", True) in [True, "selected"]:
-            schema.pop(prop)
-            continue
+
         if is_object_type(val):
             # call recursively in case any subproperties are deselected.
-            _pop_deselected_schema(val, catalog_entry, breadcrumb + [prop])
-
-
-def _get_metadata_by_breadcrumb(
-    catalog_entry: dict, breadcrumb: List[str]
-) -> Optional[dict]:
-    md: Optional[dict] = catalog_entry.get("metadata")
-    if not md:
-        return None
-    for md_entry in md:
-        if md_entry.get("breadcrumb", None) == breadcrumb:
-            return md_entry
-    return None
+            _pop_deselected_schema(
+                val, catalog, stream_name, property_breadcrumb, logger
+            )
--- singer_sdk/tests/core/test_catalog_selection.py
+++ singer_sdk/tests/core/test_catalog_selection.py
@@ -1,24 +1,117 @@
 """Test catalog selection features."""
 
-from singer_sdk.helpers._catalog import get_selected_schema
+import logging
+import pytest
+
+import singer
+
+from singer_sdk.helpers._catalog import get_selected_schema, is_property_selected
 from singer_sdk.typing import PropertiesList, Property, StringType, ObjectType
 
 
-def test_schema_selection():
-    """Test that schema selection rules are correctly applied to SCHEMA messages."""
-    schema = PropertiesList(
+@pytest.fixture
+def record():
+    return {
+        "col_a": {
+            "col_a_1": "something",
+            "col_a_2": "else",
+        },
+        "col_b": {
+            "col_b_1": "the answer",
+            "col_b_2": "42",
+        },
+    }
+
+
+@pytest.fixture
+def stream_name():
+    return "test_stream_a"
+
+
+@pytest.fixture
+def schema():
+    return PropertiesList(
         Property(
             "col_a",
             ObjectType(
                 Property("col_a_1", StringType),
                 Property("col_a_2", StringType),
             ),
-        )
+        ),
+        Property(
+            "col_b",
+            ObjectType(
+                Property("col_b_1", StringType),
+                Property("col_b_2", StringType),
+            ),
+        ),
     ).to_dict()
-    catalog_entry = {}  # TODO: construct a filtering catalog dict, excluding col_a_2
-    selected_schema = get_selected_schema(schema, catalog_entry)
+
+
+@pytest.fixture
+def catalog_entry_obj(schema, stream_name) -> singer.CatalogEntry:
+    return singer.CatalogEntry(
+        tap_stream_id=stream_name,
+        stream=stream_name,
+        schema=singer.Schema.from_dict(schema),
+        metadata=[
+            {
+                "breadcrumb": (),
+                "metadata": {
+                    "selected": True,
+                },
+            },
+            {
+                "breadcrumb": ("col_a", "col_a_2"),
+                "metadata": {
+                    "selected": False,  # Should not be overridden by parent
+                },
+            },
+            {
+                "breadcrumb": ("col_b",),
+                "metadata": {
+                    "selected": False,
+                },
+            },
+            {
+                "breadcrumb": ("col_b", "col_b_1"),
+                "metadata": {
+                    "selected": True,  # Should be overridden by parent
+                },
+            },
+        ],
+    )
+
+
+@pytest.fixture
+def catalog_entry_dict(catalog_entry_obj) -> dict:
+    return catalog_entry_obj.to_dict()
+
+
+@pytest.fixture
+def catalog(catalog_entry_obj):
+    return singer.Catalog(streams=[catalog_entry_obj]).to_dict()
+
+
+@pytest.fixture
+def selection_test_cases():
+    return [
+        ((), True),
+        (("col_a",), True),
+        (("col_a", "col_a_1"), True),
+        (("col_a", "col_a_2"), False),
+        (("col_b",), False),
+        (("col_b", "col_b_1"), False),
+        (("col_b", "col_b_2"), False),
+    ]
+
+
+def test_schema_selection(schema, catalog, stream_name):
+    """Test that schema selection rules are correctly applied to SCHEMA messages."""
+    selected_schema = get_selected_schema(catalog, stream_name, logging.getLogger())
+    # selected_schema["properties"]["required"] = []
     assert (
-        selected_schema
+        selected_schema["properties"]
         == PropertiesList(
             Property(
                 "col_a",
@@ -26,5 +119,21 @@ def test_schema_selection():
                     Property("col_a_1", StringType),
                 ),
             )
-        ).to_dict()
+        ).to_dict()["properties"]
     )
+
+
+def test_record_selection(
+    record, catalog_entry_dict, stream_name, selection_test_cases
+):
+    """Test that record selection rules are correctly applied to SCHEMA messages."""
+    for bookmark, expected in selection_test_cases:
+        assert (
+            is_property_selected(
+                catalog,
+                stream_name,
+                breadcrumb=bookmark,
+                logger=logging.getLogger(),
+            )
+            == expected
+        ), f"bookmark {bookmark} was expected to be selected={expected}"
