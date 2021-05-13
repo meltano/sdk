c71e0a6550c5d65cc671d0f1c4b6f4ad78a82407
Edgar R. Mondragón <edgarrm358@gmail.com>
2021-05-12 19:46:53 -0500

    Remove extra whitespace

--- singer_sdk/samples/sample_tap_gitlab/gitlab_tap.py
+++ singer_sdk/samples/sample_tap_gitlab/gitlab_tap.py
@@ -37,8 +37,6 @@ class SampleTapGitlab(Tap):
         group_ids: List[str]
         start_date: datetime
 
-
-
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
