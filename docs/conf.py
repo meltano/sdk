475891ed3e2175f8533c8a0048447085b8508f66
AJ Steers <aaronsteers@gmail.com>
2021-06-03 18:53:30 -0700

    add sphinx plugins as extas

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../singer_sdk"))
+# sys.path.insert(0, os.path.abspath("../singer_sdk"))
 # sys.path.insert(0, os.path.abspath("/Users/ajsteers/Source/singer-sdk"))
 
 
