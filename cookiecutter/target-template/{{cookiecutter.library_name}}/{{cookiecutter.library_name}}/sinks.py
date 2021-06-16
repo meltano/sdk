e9e668f532044c57e74a1d0ada4200e5e2d95412
AJ Steers <aaronsteers@gmail.com>
2021-06-16 13:16:25 -0700

    updated target cookiecutter

--- cookiecutter/target-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/sinks.py
+++ cookiecutter/target-template/{{cookiecutter.library_name}}/{{cookiecutter.library_name}}/sinks.py
@@ -2,29 +2,30 @@
 
 from typing import Any, Dict, List, Tuple, Union
 
-from singer_sdk.sinks import Sink
+{% set sinkclass = "BatchSink" if cookiecutter.serialization_method == "Per batch" else "RecordSink" %}
+from singer_sdk.sinks import {{ sinkclass }}
 
 
-class {{ cookiecutter.destination_name }}Sink(Sink):
+class {{ cookiecutter.destination_name }}Sink({{ sinkclass }}):
     """{{ cookiecutter.destination_name }} target sink class."""
-
+    {% if sinkclass == "BatchSink" %}
     DEFAULT_BATCH_SIZE_ROWS = 10000
+    {%- endif %}
 
-    def load_record(self, record: dict) -> None:
-        """Load a record."""
-        # TODO: Load
+    def process_record(self, record: dict) -> None:
+        """Process the record."""
         # Sample:
         # for record in records_to_drain:
         #     write(record)
         #     count += 1
         # self.tally_record_written(count)
 
-    def drain(self, records_to_drain: Union[List[dict], Any]) -> None:
-        """Write any prepped records out and return only once fully written."""
-        # TODO: If records are not written or finalized during load_record(),
-        #       write them in batch.
+    {% if sinkclass == "BatchSink" -%}
+    def process_batch(self, records_to_drain: Union[List[dict], Any]) -> None:
+        """Write out any prepped records and return once fully written."""
         # Sample:
         # for record in records_to_drain:
         #     write(record)
         #     count += 1
         # self.tally_record_written(count)
+    {%- endif %}
