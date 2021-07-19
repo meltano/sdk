c0b605ee556624d47247abe4fc3d4529d66f54eb
edward <esmith@stardotstar.org>
2021-07-19 17:54:53 -0400

    Support for `initial_replication_key_value` in meltano.yml config.  Fixes https://gitlab.com/meltano/meltano/issues/2734
    Support for `initial_replication_key_value` in meltano.yml config.  Fixes https://gitlab.com/meltano/meltano/issues/2734

--- singer_sdk/streams/core.py
+++ singer_sdk/streams/core.py
@@ -216,10 +216,24 @@ class Stream(metaclass=abc.ABCMeta):
         if self.replication_key:
             state = self.get_context_state(context)
             replication_key_value = state.get("replication_key_value")
-            if replication_key_value and self.replication_key == state.get(
-                "replication_key"
-            ):
-                return replication_key_value
+
+            if replication_key_value:
+                """Return the replication_key_value from the state"""
+                state_replication_key = state.get("replication_key")
+                if self.replication_key == state_replication_key:
+                    return replication_key_value
+                else:
+                    self.logger.warning(
+                        f"{self.name} replication_key changed from '{state_replication_key}'"
+                        "to '{self.replication_key}'."
+                    )
+                    return None
+            else:
+                """Return the initial_replication_key_value if defined"""
+                md_map = singer.metadata.to_map(self.metadata)
+                md_entry = md_map.get(())
+                if "initial_replication_key_value" in md_entry:
+                    return md_entry.get("initial_replication_key_value")
 
         return None
 
