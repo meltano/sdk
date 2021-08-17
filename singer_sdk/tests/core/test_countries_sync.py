9b53482fa20df738d8b32d74e65f905a159aaf31
Edgar R. Mondragón <edgarrm358@gmail.com>
2021-08-17 22:25:45 +0000

    Select all streams when there's no input catalog

--- singer_sdk/tests/core/test_countries_sync.py
+++ singer_sdk/tests/core/test_countries_sync.py
@@ -1,5 +1,9 @@
 """Test sample sync."""
 
+import copy
+import logging
+
+from singer_sdk.helpers._catalog import get_selected_schema, pop_deselected_record_properties
 from singer_sdk.samples.sample_tap_countries.countries_tap import SampleTapCountries
 
 
@@ -31,7 +35,49 @@ def test_countries_primary_key():
 
 def test_with_catalog_mismatch():
     """Test catalog apply with no matching stream catalog entries."""
-    tap = SampleTapCountries(config=None, catalog={"streams": {}})
+    tap = SampleTapCountries(config=None, catalog={"streams": []})
     for stream in tap.streams.values():
         # All streams should be deselected:
         assert not stream.selected
+
+
+def test_with_catalog_entry():
+    """Test catalog apply with a matching stream catalog entry for one stream."""
+    tap = SampleTapCountries(
+        config=None,
+        catalog={
+            "streams": [
+                {
+                    "tap_stream_id": "continents",
+                    "schema": {
+                        "type": "object",
+                        "properties": {},
+                    },
+                    "metadata": [],
+                },
+            ],
+        },
+    )
+    assert tap.streams["continents"].selected
+    assert not tap.streams["countries"].selected
+
+    stream = tap.streams["continents"]
+    record = next(stream.get_records(None))
+    copied_record = copy.deepcopy(record)
+
+    pop_deselected_record_properties(
+        record=copied_record,
+        schema=stream.schema,
+        metadata=stream.metadata,
+        stream_name=stream.name,
+        logger=logging.getLogger(),
+    )
+    assert copied_record == record
+
+    new_schema = get_selected_schema(
+        stream_name=stream.name,
+        schema=stream.schema,
+        metadata=stream.metadata,
+        logger=logging.getLogger(),
+    )
+    assert new_schema == stream.schema
