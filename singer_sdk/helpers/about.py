b613f67ac940d6c9430498c4667d4666f698861e
AJ Steers <aj@meltano.com>
2021-08-24 14:50:09 -0700

    add comments

--- singer_sdk/helpers/about.py
+++ singer_sdk/helpers/about.py
@@ -7,31 +7,33 @@ class PluginCapabilities(str, Enum):
     """Core capabilities which can be supported by taps and targets."""
 
     # Supported by default:
-    ABOUT = "about"
-    STREAM_MAPS = "stream-maps"
+    ABOUT = "about"  # Support capability and setting discovery
+    STREAM_MAPS = "stream-maps"  # Support inline stream map transforms
 
     # Not supported by default:
-    ACTIVATE_VERSION = "activate-version"
+    ACTIVATE_VERSION = "activate-version"  # Support the ACTIVATE_VERSION extension
 
 
 class TapCapabilities(PluginCapabilities, Enum):
     """Tap-specific capabilities."""
 
     # Supported by default:
-    CATALOG = "catalog"
-    DISCOVER = "discover"
-    STATE = "state"
+
+    DISCOVER = "discover"  # Generate a catalog with `--discover`
+    CATALOG = "catalog"  # Accept input catalog, apply metadata and selection rules
+    STATE = "state"  # Incremental refresh by means of state tracking
+    TEST = "test"  # Automatic connectivity and stream init test via `--test`
 
     # Not supported by default:
-    LOG_BASED = "log-based"
-
-    # Deprecated. Please use 'catalog' instead:
-    PROPERTIES = "properties"
+    LOG_BASED = "log-based"  # Support for `replication_method='LOG_BASED'`
+    PROPERTIES = "properties"  # Deprecated. Please use 'catalog' instead
 
 
 class TargetCapabilities(PluginCapabilities, Enum):
     """Target-specific capabilities."""
 
     # Not supported by default:
-    SOFT_DELETE = "soft-delete"
-    HARD_DELETE = "hard-delete"
+    SOFT_DELETE = "soft-delete"  # Allows a `soft_delete=True` config option
+    HARD_DELETE = "hard-delete"  # Allows a `hard_delete=True` config option
+    DATATYPE_FAILSAFE = "datatype-failsafe"  # Fail safe for unknown JSON Schema types
+    RECORD_FLATTENING = "record-flattening"  # Allow denesting complex properties
