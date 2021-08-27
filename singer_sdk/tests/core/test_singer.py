c8f69e70863750ef94f5854f375dbd2ed4e450ff
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-08-27 13:52:13 -0500

    Remove unnecessary catalog parsing tests

--- singer_sdk/tests/core/test_singer.py
+++ singer_sdk/tests/core/test_singer.py
@@ -1,58 +1,8 @@
-from singer_sdk.helpers._singer import Catalog, CatalogEntry, MetadataMapping
-
-
-def test_1():
-    metadata_array = [
-        {
-            "breadcrumb": [],
-            "metadata": {
-                "inclusion": "available",
-            },
-        },
-        {
-            "breadcrumb": ["a"],
-            "metadata": {
-                "inclusion": "unsupported",
-            },
-        },
-    ]
-    mapping = MetadataMapping.from_iterable(metadata_array)
-    assert mapping[()] == {"inclusion": "available"}
-    assert mapping[("a",)] == {"inclusion": "unsupported"}
-
-    assert mapping.to_list() == metadata_array
-
-
-def test_2():
-    entry_dict = {
-        "tap_stream_id": "test",
-        "metadata": [
-            {
-                "breadcrumb": [],
-                "metadata": {
-                    "inclusion": "available",
-                },
-            },
-            {
-                "breadcrumb": ["a"],
-                "metadata": {
-                    "inclusion": "unsupported",
-                },
-            },
-        ],
-        "schema": {
-            "type": "object",
-        },
-    }
-    entry = CatalogEntry.from_dict(entry_dict)
-
-    assert entry.metadata.to_list() == entry_dict["metadata"]
-    assert entry.tap_stream_id == entry_dict["tap_stream_id"]
-    assert entry.schema.to_dict() == {"type": "object"}
-    assert entry.to_dict() == entry_dict
+from singer_sdk.helpers._singer import Catalog, CatalogEntry
 
 
 def test_catalog_parsing():
+    """Validate parsing works for a catalog and its stream entries."""
     catalog_dict = {
         "streams": [
             {
