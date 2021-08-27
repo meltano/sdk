1d51d48d6546631c73c04e99fc119e042a2753fb
AJ Steers <aj@meltano.com>
2021-08-26 18:26:32 -0700

    resolve merge conflicts

--- singer_sdk/streams/database.py
+++ singer_sdk/streams/database.py
@@ -9,11 +9,6 @@ import backoff
 
 import singer
 from singer.schema import Schema
-from singer_sdk.helpers.util import (
-    get_catalog_entries,
-    get_catalog_entry_name,
-    get_catalog_entry_schema,
-)
 from singer_sdk.helpers.classproperty import classproperty
 from singer_sdk.exceptions import TapStreamConnectionFailure
 from typing import Any, Dict, Iterable, List, Optional, Tuple, TypeVar, Union, cast
@@ -49,6 +44,37 @@ SINGER_TYPE_LOOKUP = {
     "bool": SINGER_BOOLEAN_TYPE,
     "variant": SINGER_STRING_TYPE,  # TODO: Support nested objects.
 }
+
+
+def get_catalog_entries(catalog_dict: dict) -> List[dict]:
+    """Parse the catalog dict and return a list of catalog entries."""
+    if "streams" not in catalog_dict:
+        raise ValueError("Catalog does not contain expected 'streams' collection.")
+    if not catalog_dict.get("streams"):
+        raise ValueError("Catalog does not contain any streams.")
+    return cast(List[dict], catalog_dict.get("streams"))
+
+
+def get_catalog_entry_name(catalog_entry: dict) -> str:
+    """Return the name of the provided catalog entry dict."""
+    result = catalog_entry.get("stream", catalog_entry.get("tap_stream_id", None))
+    if not result:
+        raise ValueError(
+            "Stream name could not be identified due to missing or blank"
+            "'stream' and 'tap_stream_id' values."
+        )
+    return result
+
+
+def get_catalog_entry_schema(catalog_entry: dict) -> dict:
+    """Return the JSON Schema dict for the specified catalog entry dict."""
+    result = catalog_entry.get("schema", None)
+    if not result:
+        raise ValueError(
+            "Stream does not have a valid schema. Please check that the catalog file "
+            "is properly formatted."
+        )
+    return result
 
 
 class DatabaseStream(Stream, metaclass=abc.ABCMeta):
