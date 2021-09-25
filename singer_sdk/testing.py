080f8e054c81ec69f6f134125c8280016f588d09
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-09-25 16:26:02 -0500

    Add annotations and dosctrings to singer_sdk/testing.py

--- singer_sdk/testing.py
+++ singer_sdk/testing.py
@@ -5,10 +5,18 @@ from typing import Type, Callable, List
 from singer_sdk.tap_base import Tap
 
 
-def get_standard_tap_tests(tap_class: Type[Tap], config=None) -> List[Callable]:
-    """Return callable pytest which executes simple discovery and connection tests."""
+def get_standard_tap_tests(tap_class: Type[Tap], config: dict = None) -> List[Callable]:
+    """Return callable pytest which executes simple discovery and connection tests.
 
-    def _test_cli_prints():
+    Args:
+        tap_class: TODO
+        config: TODO
+
+    Returns:
+        TODO
+    """
+
+    def _test_cli_prints() -> None:
         # Initialize with basic config
         tap1: Tap = tap_class(config=config, parse_env_config=True)
         # Test CLI prints
@@ -16,7 +24,7 @@ def get_standard_tap_tests(tap_class: Type[Tap], config=None) -> List[Callable]:
         tap1.print_about()
         tap1.print_about(format="json")
 
-    def _test_discovery():
+    def _test_discovery() -> tap_class:
         # Initialize with basic config
         tap1: Tap = tap_class(config=config, parse_env_config=True)
         # Test discovery
@@ -27,7 +35,7 @@ def get_standard_tap_tests(tap_class: Type[Tap], config=None) -> List[Callable]:
         tap2: Tap = tap_class(config=config, parse_env_config=True, catalog=catalog1)
         assert tap2
 
-    def _test_stream_connections():
+    def _test_stream_connections() -> None:
         # Initialize with basic config
         tap1: Tap = tap_class(config=config, parse_env_config=True)
         tap1.run_connection_test()
@@ -35,6 +43,17 @@ def get_standard_tap_tests(tap_class: Type[Tap], config=None) -> List[Callable]:
     return [_test_cli_prints, _test_discovery, _test_stream_connections]
 
 
-def get_standard_target_tests(target_class: Type[Tap], config=None) -> List[Callable]:
-    """Return callable pytest which executes simple discovery and connection tests."""
+def get_standard_target_tests(
+    target_class: Type[Tap],
+    config: dict = None,
+) -> List[Callable]:
+    """Return callable pytest which executes simple discovery and connection tests.
+
+    Args:
+        target_class: TODO
+        config: TODO
+
+    Returns:
+        TODO
+    """
     return []
