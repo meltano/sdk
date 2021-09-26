5e2bc5ea828127ded62a0560882aeca941db6721
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-09-25 19:36:11 -0500

    Add annotations and dosctrings to singer_sdk/helpers/jsonpath.py

--- singer_sdk/helpers/jsonpath.py
+++ singer_sdk/helpers/jsonpath.py
@@ -9,7 +9,15 @@ import jsonpath_ng
 def extract_jsonpath(
     expression: str, input: Union[dict, list]
 ) -> Generator[Any, None, None]:
-    """Extract records from an input based on a JSONPath expression."""
+    """Extract records from an input based on a JSONPath expression.
+
+    Args:
+        expression: JSONPath expression to match against the input.
+        input: JSON object or array to extract records from.
+
+    Yields:
+        Records matched with JSONPath expression.
+    """
     compiled_jsonpath = _compile_jsonpath(expression)
 
     match: jsonpath_ng.DatumInContext
@@ -19,5 +27,12 @@ def extract_jsonpath(
 
 @memoization.cached
 def _compile_jsonpath(expression: str) -> jsonpath_ng.JSONPath:
-    """Parse a JSONPath expression and cache the result."""
+    """Parse a JSONPath expression and cache the result.
+
+    Args:
+        expression: A string representing a JSONPath expression.
+
+    Returns:
+        A compiled JSONPath object.
+    """
     return jsonpath_ng.parse(expression)
