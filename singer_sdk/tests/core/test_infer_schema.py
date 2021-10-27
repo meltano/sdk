404f26cf1edcfbde32fb43310f72b36f008f068c
Josh Lloyd <jlloyd@jlloyd-mbp19.local>
2021-10-27 16:31:10 -0600

    added parent/child test

--- singer_sdk/tests/core/test_infer_schema.py
+++ singer_sdk/tests/core/test_infer_schema.py
@@ -1,4 +1,5 @@
 """Infer schema tests."""
+import json
 from pathlib import Path
 from typing import List, Optional
 
@@ -11,10 +12,15 @@ from singer_sdk.typing import NumberType, PropertiesList, Property
 
 resource_path = Path(__file__).parent / "resources"
 
+# test data structures
 
-def request_with_backoff():
+basic = [{"col1": "val1"}, {"col2": 10}]
+
+
+def request_with_backoff(content=basic) -> requests.Response:
+    """Mock request fucntion with variable data."""
     fake_response = requests.Response()
-    fake_response._content = str.encode('[{"col1": "val1"}, {"col2": 10}]')
+    fake_response._content = str.encode(json.dumps(content))
     return fake_response
 
 
@@ -220,7 +226,6 @@ def test_infer_schema_rest_limit_records():
     tap = InferSchemaTap()
     stream = tap.streams["records_only"]
 
-    print(stream.schema)
     assert stream.schema == {
         "properties": {"col1": {"type": "string"}},
         "required": ["col1"],
@@ -263,7 +268,6 @@ def test_infer_schema_rest_records_by_config():
     tap = InferSchemaTap()
     stream = tap.streams["records_only"]
 
-    print(stream.schema)
     assert stream.schema == {
         "properties": {"col1": {"type": "string"}},
         "required": ["col1"],
@@ -310,9 +314,77 @@ def test_infer_schema_rest_records_by_config_stream():
     tap = InferSchemaTap()
     stream = tap.streams["records_only"]
 
-    print(stream.schema)
     assert stream.schema == {
         "properties": {"col1": {"type": "string"}},
         "required": ["col1"],
+        "type": "object",
+    }
+
+
+def test_infer_schema_child_stream():
+    """Validate schema inference for child streams."""
+
+    class ParentStream(RESTStream):
+        """Test RESTful stream class."""
+
+        name = "parent"
+        path = "/example"
+        url_base = "https://example.com"
+        primary_keys = ["col1"]
+        schema = {}
+        schema_inference_record_count = 2
+
+        def _request_with_backoff(
+            self, prepared_request: requests.PreparedRequest, context: Optional[dict]
+        ) -> requests.Response:
+            return request_with_backoff([{"id": 1}, {"value": "foo"}])
+
+        def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
+            """Return a context dictionary for child streams."""
+            return {"parent_id": record["id"]}
+
+    class ChildStream(RESTStream):
+        """Test RESTful stream class."""
+
+        parent_stream_type = ParentStream
+        ignore_parent_replication_keys = True
+
+        name = "child"
+        path = "/example"
+        url_base = "https://example.com"
+        primary_keys = ["col1"]
+        schema = {}
+        schema_inference_record_count = 1
+
+        def _request_with_backoff(
+            self, prepared_request: requests.PreparedRequest, context: Optional[dict]
+        ) -> requests.Response:
+            return request_with_backoff(
+                [
+                    {"parent_id": 1, "toy": "bar"},
+                    {"parent_id": 2, "toy": "spam"},
+                ]
+            )
+
+    class InferSchemaTap(Tap):
+        """Test tap class."""
+
+        name = "test-tap"
+
+        def discover_streams(self) -> List[Stream]:
+            """List all streams."""
+            return [ParentStream(self), ChildStream(self)]
+
+    tap = InferSchemaTap()
+    parent = tap.streams["parent"]
+    child = tap.streams["child"]
+
+    assert parent.schema == {
+        "properties": {"id": {"type": "integer"}, "value": {"type": "string"}},
+        "type": "object",
+    }
+    assert child.schema == {
+        "properties": {"parent_id": {"type": "integer"}, "toy": {"type": "string"}},
+        "required": ["parent_id", "toy"],
         "type": "object",
     }
