6e81ca1714d4983c98bf6c7d97ebc7c7c317018b
Aaron ("AJ") Steers <aj@meltano.com>
2021-11-09 01:02:14 -0800

    lint fixes

--- singer_sdk/tests/core/test_sqlite.py
+++ singer_sdk/tests/core/test_sqlite.py
@@ -7,7 +7,7 @@ from typing import cast
 import pytest
 
 from singer_sdk import SQLConnector, SQLStream
-from singer_sdk.helpers._singer import Metadata, MetadataMapping, StreamMetadata
+from singer_sdk.helpers._singer import MetadataMapping, StreamMetadata
 from singer_sdk.tap_base import SQLTap
 
 
