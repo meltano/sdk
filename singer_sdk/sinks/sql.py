924f83c62f56df372228d87f19ab87d0ad96b5e7
Aaron ("AJ") Steers <aj@meltano.com>
2021-11-09 00:55:05 -0800

    continued sink iteration

--- singer_sdk/sinks/sql.py
+++ singer_sdk/sinks/sql.py
@@ -1,12 +1,10 @@
 """Sink classes load data to SQL targets."""
 
-from enum import Enum
-from typing import Any, Dict, Iterable, List, Optional
+from typing import Any, Dict, Iterable, List, Optional, Type
 
 import sqlalchemy
 from pendulum import now
 
-from singer_sdk import typing as th
 from singer_sdk.plugin_base import PluginBase
 from singer_sdk.sinks.batch import BatchSink
 from singer_sdk.streams.sql import SQLConnector
@@ -15,15 +13,9 @@ from singer_sdk.streams.sql import SQLConnector
 class SQLSink(BatchSink):
     """SQL-type sink type."""
 
-    connector: SQLConnector
+    connector_class: Type[SQLConnector]
     soft_delete_column_name = "_sdc_deleted_at"
     version_column_name = "_sdc_table_version"
-
-    class TableType(Enum):
-        """Controls the available table types for a connector."""
-
-        PERMANENT: str = "PERMANENT"
-        TEMPORARY: str = "TEMPORARY"
 
     def __init__(
         self,
@@ -31,6 +23,7 @@ class SQLSink(BatchSink):
         stream_name: str,
         schema: Dict,
         key_properties: Optional[List[str]],
+        connector: Optional[SQLConnector] = None,
     ) -> None:
         """Initialize SQL Sink.
 
@@ -39,8 +32,24 @@ class SQLSink(BatchSink):
             stream_name: The source tap's stream name.
             schema: The JSON Schema definition.
             key_properties: The primary key columns.
+            connector: Optional connector to reuse.
         """
+        self._connector: SQLConnector
+        if connector:
+            self._connector = connector
+        else:
+            self._connector = self.connector_class(dict(target.config))
+
         super().__init__(target, stream_name, schema, key_properties)
+
+    @property
+    def connector(self) -> SQLConnector:
+        """The connector object.
+
+        Returns:
+            The connector object.
+        """
+        return self._connector
 
     @property
     def connection(self) -> sqlalchemy.engine.Connection:
@@ -85,15 +94,14 @@ class SQLSink(BatchSink):
         """
         # If duplicates are merged, these can be tracked via
         # :meth:`~singer_sdk.Sink.tally_duplicate_merged()`.
-
-        if not self.target_table_exists:
-            self.create_table_from_records(context["records"])
-            return
-
-        self.prepare_target_table(
+        self.connector.prepare_table(
             full_table_name=self.full_table_name,
             schema=self.schema,
             primary_keys=self.key_properties,
+            as_temp_table=False,
+        )
+        self.bulk_insert_records(
+            full_table_name=self.full_table_name, records=context["records"]
         )
 
     @property
@@ -109,35 +117,6 @@ class SQLSink(BatchSink):
             self.database_name,
         )
 
-    def target_table_exists(
-        self, full_table_name: Optional[str] = None
-    ) -> Optional[bool]:
-        """Determine if the target table already exists.
-
-        Args:
-            full_table_name: the target table name.
-
-        Returns:
-            True if table exists, False if not, None if unsure or undetectable.
-        """
-        return None
-
-    def target_column_exists(
-        self,
-        full_table_name: Optional[str] = None,
-        column_name: Optional[str] = None,
-    ) -> Optional[bool]:
-        """Determine if the target table already exists.
-
-        Args:
-            full_table_name: the target table name.
-            column_name: the target column name.
-
-        Returns:
-            True if table exists, False if not, None if unsure or undetectable.
-        """
-        return None
-
     def create_table_with_records(
         self,
         full_table_name: Optional[str],
@@ -145,7 +124,7 @@ class SQLSink(BatchSink):
         records: Iterable[Dict[str, Any]],
         primary_keys: Optional[List[str]] = None,
         partition_keys: Optional[List[str]] = None,
-        table_type: TableType = TableType.PERMANENT,
+        as_temp_table: bool = False,
     ) -> None:
         """Create an empty table.
 
@@ -155,17 +134,17 @@ class SQLSink(BatchSink):
             records: records to load.
             primary_keys: list of key properties.
             partition_keys: list of partition keys.
-            table_type: permanent, temporary, or a proprietary type.
+            as_temp_table: True to create a temp table.
         """
         full_table_name = full_table_name or self.full_table_name
         if primary_keys is None:
             primary_keys = self.key_properties
         partition_keys = partition_keys or None
-        self.prepare_target_table(
+        self.connector.prepare_table(
             full_table_name=full_table_name,
             primary_keys=primary_keys,
             schema=schema,
-            table_type=table_type,
+            as_temp_table=as_temp_table,
         )
         self.bulk_insert_records(full_table_name=full_table_name, records=records)
 
@@ -199,96 +178,6 @@ class SQLSink(BatchSink):
         """
         pass
 
-    def prepare_target_table(
-        self,
-        full_table_name: str,
-        schema: dict,
-        primary_keys: List[str],
-        partition_keys: Optional[List[str]] = None,
-        table_type: TableType = TableType.PERMANENT,
-    ) -> None:
-        """Adapt target table to provided schema if possible.
-
-        Args:
-            full_table_name: the target table name.
-            schema: the import schema.
-            primary_keys: list of key properties.
-            partition_keys: list of partition keys.
-            table_type: permanent, temporary, or a proprietary type.
-        """
-        if not self.target_table_exists(full_table_name=full_table_name):
-            self.create_empty_table(
-                full_table_name=full_table_name,
-                schema=schema,
-                primary_keys=primary_keys,
-                partition_keys=partition_keys,
-                table_type=table_type,
-            )
-            return
-
-        for property_name, property_def in schema["properties"].items():
-            self.prepare_target_column(full_table_name, property_name, property_def)
-
-    def prepare_target_column(
-        self,
-        full_table_name: str,
-        column_name: str,
-        sql_type: sqlalchemy.types.TypeEngine,
-    ) -> None:
-        """Adapt target table to provided schema if possible.
-
-        Args:
-            full_table_name: the target table name.
-            column_name: the target column name.
-            schema: the import schema.
-        """
-        if not self.target_column_exists(full_table_name, column_name):
-            self.create_empty_column(
-                full_table_name=full_table_name,
-                column_name=column_name,
-                sql_type=sql_type,
-            )
-            return
-
-        self._adapt_column_type(
-            full_table_name,
-            column_name=column_name,
-            sql_type=sql_type,
-        )
-
-    def create_empty_table(
-        self,
-        full_table_name: str,
-        schema: dict,
-        primary_keys: Optional[List[str]] = None,
-        partition_keys: Optional[List[str]] = None,
-        table_type: TableType = TableType.PERMANENT,
-    ) -> None:
-        """Create an empty target table.
-
-        Args:
-            full_table_name: the target table name.
-            schema: the JSON schema for the new table.
-            primary_keys: list of key properties.
-            partition_keys: list of partition keys.
-            table_type: permanent, temporary, or a proprietary type.
-        """
-        pass
-
-    def create_empty_column(
-        self,
-        full_table_name: str,
-        column_name: str,
-        sql_type: sqlalchemy.types.TypeEngine,
-    ) -> None:
-        """Create a new column.
-
-        Args:
-            full_table_name: The target table name.
-            column_name: The name of the new column.
-            schema: JSON Schema type definition to be used in creating the new column.
-        """
-
     def activate_table_version(
         self,
         full_table_name: str,
@@ -305,10 +194,12 @@ class SQLSink(BatchSink):
                 f"DELETE FROM {self.full_table_name} "
                 f"WHERE {self.version_column_name} <= {table_version}"
             )
-        if not self.target_column_exists(
+            return
+
+        if not self.connector.column_exists(
             full_table_name=full_table_name, column_name=self.soft_delete_column_name
         ):
-            self.prepare_target_column(
+            self.connector.prepare_column(
                 self.full_table_name,
                 self.soft_delete_column_name,
                 sql_type=sqlalchemy.types.DateTime,
@@ -316,27 +207,4 @@ class SQLSink(BatchSink):
         self.connection.execute(
             f"UPDATE {self.full_table_name} "
             f"SET {self.soft_delete_column_name} = '{now().isoformat()}'"
-        )
-
-    def _adapt_column_type(
-        self,
-        full_table_name: str,
-        column_name: str,
-        sql_type: sqlalchemy.types.TypeEngine,
-    ) -> None:
-        """Adapt table column type to support the new JSON schema type.
-
-        Args:
-            full_table_name: The target table name.
-            column_name: The target column name.
-            sql_type: The new SQLAlchemy type.
-        """
-        current_sql_type = self.connector.get_column_type(full_table_name, column_name)
-        needed_sql_type = self.connector.to_sql_type(sql_type)
-        compatible_sql_type = self.connector.merge_sql_types(
-            current_sql_type, needed_sql_type
-        )
-        self.connection.execute(
-            f"ALTER TABLE {full_table_name} "
-            f"ALTER COLUMN {column_name} ({compatible_sql_type})"
         )
