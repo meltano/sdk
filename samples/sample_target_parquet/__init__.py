9e89717bd322d3eec6bcbfa9213deb940c27ea76
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-11-26 13:45:14 -0600

    fix: Move samples and tests to repo root
    https://gitlab.com/meltano/sdk/-/issues/280

--- samples/sample_target_parquet/__init__.py
+++ samples/sample_target_parquet/__init__.py
@@ -2,10 +2,8 @@
 
 
 # Reuse the tap connection rather than create a new target connection:
-from singer_sdk.samples.sample_target_parquet.parquet_target import SampleTargetParquet
-from singer_sdk.samples.sample_target_parquet.parquet_target_sink import (
-    SampleParquetTargetSink,
-)
+from samples.sample_target_parquet.parquet_target import SampleTargetParquet
+from samples.sample_target_parquet.parquet_target_sink import SampleParquetTargetSink
 
 __all__ = [
     "SampleTargetParquet",
