b532ccb91045d19d7c8eed45915761e95ec3a31c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-12-13 12:42:34 -0600

    test: add standard metadata tests

--- tests/core/test_singer.py
+++ tests/core/test_singer.py
@@ -1,4 +1,8 @@
-from singer_sdk.helpers._singer import Catalog, CatalogEntry
+from typing import List
+
+import pytest
+
+from singer_sdk.helpers._singer import Catalog, CatalogEntry, Metadata, MetadataMapping
 
 
 def test_catalog_parsing():
@@ -44,3 +48,54 @@ def test_catalog_parsing():
     entry = CatalogEntry.from_dict(new)
     catalog.add_stream(entry)
     assert catalog.get_stream("new") == entry
+
+
+@pytest.mark.parametrize(
+    "schema,key_properties,replication_method",
+    [
+        (
+            {"properties": {"id": {"type": "integer"}}, "type": "object"},
+            ["id"],
+            "FULL_TABLE",
+        ),
+        (
+            {
+                "properties": {
+                    "first_name": {"type": "string"},
+                    "last_name": {"type": "string"},
+                },
+                "type": "object",
+            },
+            ["first_name", "last_name"],
+            "FULL_TABLE",
+        ),
+        (
+            {},
+            [],
+            None,
+        ),
+    ],
+)
+def test_standard_metadata(
+    schema: dict,
+    key_properties: List[str],
+    replication_method: str,
+):
+    """Validate generated metadata."""
+    metadata = MetadataMapping.get_standard_metadata(
+        schema=schema,
+        schema_name="test",
+        key_properties=key_properties,
+        replication_method=replication_method,
+    )
+
+    stream_metadata = metadata[()]
+    assert stream_metadata.table_key_properties == key_properties
+    assert stream_metadata.forced_replication_method == replication_method
+    assert stream_metadata.valid_replication_keys is None
+    assert stream_metadata.selected is None
+
+    for pk in key_properties:
+        pk_metadata = metadata[("properties", pk)]
+        assert pk_metadata.inclusion == Metadata.InclusionType.AUTOMATIC
+        assert pk_metadata.selected is None
