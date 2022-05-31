3596977c7dad7e9a9f943c9fa4f07fa329e66c30
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-05-30 22:38:18 -0500

    test: Mark all tests under tests/external*/ as 'external'

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
 """Top level test fixtures."""
 
+from __future__ import annotations
+
 import os
+import pathlib
 import shutil
 
 import pytest
+from _pytest.config import Config
+
+
+def pytest_collection_modifyitems(config: Config, items: list[pytest.Item]):
+    rootdir = pathlib.Path(config.rootdir)
+
+    for item in items:
+        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
+
+        # Mark all tests under tests/external*/ as 'external'
+        if rel_path.parts[1].startswith("external"):
+            item.add_marker("external")
 
 
 @pytest.fixture(scope="class")
