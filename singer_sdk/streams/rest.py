adc16435d04a7fabd4bfd19b396b86c9a962c528
Eric Boucher <eric.p.boucher@gmail.com>
2022-07-02 11:22:20 +0200

    Output full path in error messages

--- singer_sdk/streams/rest.py
+++ singer_sdk/streams/rest.py
@@ -16,6 +16,7 @@ from typing import (
     TypeVar,
     Union,
 )
+from urllib.parse import urlparse
 
 import backoff
 import requests
@@ -186,6 +187,7 @@ class RESTStream(Stream, Generic[_TToken], metaclass=abc.ABCMeta):
         Returns:
             str: The error message
         """
+        full_path = urlparse(response.url).path
         if 400 <= response.status_code < 500:
             error_type = "Client"
         else:
@@ -193,7 +195,7 @@ class RESTStream(Stream, Generic[_TToken], metaclass=abc.ABCMeta):
 
         return (
             f"{response.status_code} {error_type} Error: "
-            f"{response.reason} for path: {self.path}"
+            f"{response.reason} for path: {full_path}"
         )
 
     def request_decorator(self, func: Callable) -> Callable:
