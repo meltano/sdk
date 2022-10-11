c7abd72b5bdf9d9e20c5ca74197ac68e4acf92c2
Ken Payne <ken@meltano.com>
2022-10-11 16:21:28 +0100

    undo connection module

--- singer_sdk/sql/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Module for helpers common to SQL streams/sinks."""
-from .connector import SQLConnector
-
-__all__ = ["SQLConnector"]
--- singer_sdk/streams/__init__.py
+++ singer_sdk/streams/__init__.py
@@ -1,10 +1,9 @@
 """SDK for building Singer taps."""
 
-from singer_sdk.sql import SQLConnector
 from singer_sdk.streams.core import Stream
 from singer_sdk.streams.graphql import GraphQLStream
 from singer_sdk.streams.rest import RESTStream
-from singer_sdk.streams.sql import SQLStream
+from singer_sdk.streams.sql import SQLConnector, SQLStream
 
 __all__ = [
     "Stream",
