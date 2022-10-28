175b6ed6d718389242e19ab0ca115957c63fc8b4
Ken Payne <ken@meltano.com>
2022-10-28 17:10:02 +0100

    fix broken legacy imports

--- singer_sdk/testing/__init__.py
+++ singer_sdk/testing/__init__.py
@@ -1,8 +1,12 @@
 """SDK Standard Tap and Target Tests."""
 
-# compatibility test imports
+
 from .target import StandardSqlTargetTests
+
+# compatibility test imports
 from .testing import (
+    _get_tap_catalog,
+    _select_all,
     get_standard_tap_tests,
     get_standard_target_tests,
     sync_end_to_end,
@@ -12,11 +16,13 @@ from .testing import (
 )
 
 __all__ = [
+    "StandardSqlTargetTests",
+    "_get_tap_catalog",
+    "_select_all",
     "get_standard_tap_tests",
     "get_standard_target_tests",
     "sync_end_to_end",
     "tap_sync_test",
     "tap_to_target_sync_test",
     "target_sync_test",
-    "StandardSqlTargetTests",
 ]
