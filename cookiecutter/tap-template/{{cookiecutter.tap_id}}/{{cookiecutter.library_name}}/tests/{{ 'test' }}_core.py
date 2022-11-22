680ccd54e9dbc35c16bf49e4eebbd64c495c7a33
Ken Payne <ken@meltano.com>
2022-11-22 11:39:48 +0000

    fix cookiecutter tests

--- cookiecutter/tap-template/{{cookiecutter.tap_id}}/{{cookiecutter.library_name}}/tests/{{ 'test' }}_core.py
+++ cookiecutter/tap-template/{{cookiecutter.tap_id}}/{{cookiecutter.library_name}}/tests/{{ 'test' }}_core.py
@@ -5,7 +5,7 @@ import datetime
 from singer_sdk.testing import (
     TapTestRunner,
     get_test_class,
-    pytest_generate_tests # pytest hook function, required for standard tests
+    pytest_generate_tests  # pytest hook function, required for standard tests
 )
 from singer_sdk.testing.suites import (
     tap_stream_attribute_tests,
@@ -23,7 +23,10 @@ SAMPLE_CONFIG = {
 
 # Run standard built-in tap tests from the SDK:
 TestTap{{ cookiecutter.source_name }} = get_test_class(
-    test_runner=TapTestRunner(tap_class=Tap{{ cookiecutter.source_name }}, config=SAMPLE_CONFIG),
+    test_runner=TapTestRunner(
+        tap_class=Tap{{ cookiecutter.source_name }},
+        config=SAMPLE_CONFIG
+    ),
     test_suites=[tap_tests, tap_stream_tests, tap_stream_attribute_tests],
 )
 
