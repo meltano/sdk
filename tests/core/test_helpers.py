51040fc9d85f6fd4ba3d98f264c215ceb392aa67
Ken Payne <ken@meltano.com>
2022-12-07 17:22:05 +0200

    update docstring

--- tests/core/test_helpers.py
+++ tests/core/test_helpers.py
@@ -7,9 +7,8 @@ from singer_sdk.helpers._compat import get_project_distribution, metadata
 def test_get_project_distribution():
     """Test `get_project_distribution()`.
 
-    PyGithub is a representative example of the case where distribution name
-    does not match the package name. Any similar singer_sdk dependency could
-    be used in stead.
+    click is a representative example of a distributed package from our dependency tree.
+    Any similar singer_sdk dependency could be used in stead.
     """
     site_package_paths = site.getsitepackages()
     singer_sdk_dependency_path = Path(site_package_paths[0]) / "click" / "__init__.py"
