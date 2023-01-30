fbc27a7c4f6d9266b1d6c3787c2c7fe8381f21b4
Matt Menzenski <matt@payitgov.com>
2023-01-30 14:51:06 -0600

    whoops, fix dt->datetime

--- tests/_singerlib/test_utils.py
+++ tests/_singerlib/test_utils.py
@@ -7,7 +7,9 @@ from singer_sdk._singerlib import strftime, strptime_to_utc
 
 
 def test_small_years():
-    assert strftime(dt(90, 1, 1, tzinfo=pytz.UTC)) == "0090-01-01T00:00:00.000000Z"
+    assert (
+        strftime(datetime(90, 1, 1, tzinfo=pytz.UTC)) == "0090-01-01T00:00:00.000000Z"
+    )
 
 
 def test_round_trip():
