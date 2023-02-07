3047aa0d6b5ffa4db4ae907be8b673a39f11250d
Ken Payne <ken@meltano.com>
2023-02-07 13:29:41 +0000

    make default max_records_limit None.

--- singer_sdk/testing/config.py
+++ singer_sdk/testing/config.py
@@ -17,6 +17,6 @@ class SuiteConfig:
              no records, for named streams.
     """
 
-    max_records_limit: int | None = 5
+    max_records_limit: int | None = None
     ignore_no_records: bool = False
     ignore_no_records_for_streams: list[str] = field(default_factory=list)
