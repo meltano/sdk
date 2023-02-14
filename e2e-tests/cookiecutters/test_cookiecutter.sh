219e0777f5789ec67c5a483040a948ae09d2e049
basteln3rk <gnutzmann@flexponsive.net>
2023-02-14 08:21:57 +0000

    add missing poetry lock step; formatting

--- e2e-tests/cookiecutters/test_cookiecutter.sh
+++ e2e-tests/cookiecutters/test_cookiecutter.sh
@@ -28,10 +28,11 @@ CC_TEST_OUTPUT=$CC_BUILD_PATH/$CC_OUTPUT_DIR
 if [[ -d "$CC_TEST_OUTPUT" ]]; then
     rm -fr "$CC_TEST_OUTPUT"
 fi
-#echo cookiecutter --replay-file $REPLAY_FILE $TAP_TEMPLATE -o $CC_BUILD_PATH; exit
+
 cookiecutter --replay-file $REPLAY_FILE $TAP_TEMPLATE -o $CC_BUILD_PATH &&
     cd $CC_TEST_OUTPUT &&
     pwd &&
+    poetry lock &&
     poetry install
 
 if [[ $? -ne 0 ]]; then
@@ -41,10 +42,10 @@ fi
 # before linting, auto-fix what can be autofixed
 LIBRARY_NAME=$(ls * -d | egrep "tap|target")
 poetry run black $LIBRARY_NAME &&
-poetry run isort $LIBRARY_NAME &&
-poetry run flake8 $LIBRARY_NAME &&
-poetry run pydocstyle $LIBRARY_NAME &&
-poetry run mypy $LIBRARY_NAME
+    poetry run isort $LIBRARY_NAME &&
+    poetry run flake8 $LIBRARY_NAME &&
+    poetry run pydocstyle $LIBRARY_NAME &&
+    poetry run mypy $LIBRARY_NAME
 ##
 
 if [[ $RUN_LINT -eq 1 ]] && [[ $? -eq 0 ]]; then
