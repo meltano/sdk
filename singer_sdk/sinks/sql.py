11831604295c75e1fd19dffef7d59054146e93c6
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-03-15 18:04:46 -0600

    Test both SQLAlchemy 1 and 2

--- singer_sdk/sinks/sql.py
+++ singer_sdk/sinks/sql.py
@@ -7,6 +7,7 @@ import typing as t
 from collections import defaultdict
 from copy import copy
 from textwrap import dedent
+from typing import Any, Sequence
 
 import sqlalchemy
 from pendulum import now
@@ -216,7 +217,7 @@ class SQLSink(BatchSink):
         }
         return conformed_schema
 
-    def conform_record(self, record: dict) -> dict:
+    def conform_record(self, record: dict[str, Any]) -> dict[str, Any]:
         """Return record dictionary with property names conformed.
 
         Args:
@@ -298,7 +299,7 @@ class SQLSink(BatchSink):
         self,
         full_table_name: str,
         schema: dict,
-        records: t.Iterable[dict[str, t.Any]],
+        records: Sequence[dict[str, Any]],
     ) -> int | None:
         """Bulk insert records to an existing destination table.
 
@@ -322,11 +323,8 @@ class SQLSink(BatchSink):
         if isinstance(insert_sql, str):
             insert_sql = sqlalchemy.text(insert_sql)
 
-        conformed_records = (
-            [self.conform_record(record) for record in records]
-            if isinstance(records, list)
-            else (self.conform_record(record) for record in records)
-        )
+        conformed_records = [self.conform_record(record) for record in records]
+
         self.logger.info("Inserting with SQL: %s", insert_sql)
         with self.connector._connect() as conn, conn.begin():
             conn.execute(insert_sql, conformed_records)
