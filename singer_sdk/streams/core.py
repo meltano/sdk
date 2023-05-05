ce2abfefdb7fe88e3b469452c682885c84c6ab03
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-05-05 10:02:02 -0600

    feat: Support deselecting streams by default

--- singer_sdk/streams/core.py
+++ singer_sdk/streams/core.py
@@ -128,6 +128,9 @@ class Stream(metaclass=abc.ABCMeta):
     batch_size: int = 1000
     """Max number of records to write to each batch file."""
 
+    selected_by_default: bool = True
+    """Whether this stream is selected by default in the catalog."""
+
     def __init__(
         self,
         tap: Tap,
@@ -456,7 +459,7 @@ class Stream(metaclass=abc.ABCMeta):
         return self._primary_keys
 
     @primary_keys.setter
-    def primary_keys(self, new_value: list[str]) -> None:
+    def primary_keys(self, new_value: list[str] | None) -> None:
         """Set primary key(s) for the stream.
 
         Args:
@@ -500,7 +503,7 @@ class Stream(metaclass=abc.ABCMeta):
         return self._replication_key
 
     @replication_key.setter
-    def replication_key(self, new_value: str) -> None:
+    def replication_key(self, new_value: str | None) -> None:
         """Set replication key for the stream.
 
         Args:
@@ -560,11 +563,11 @@ class Stream(metaclass=abc.ABCMeta):
                 [self.replication_key] if self.replication_key else None
             ),
             schema_name=None,
+            selected_by_default=self.selected_by_default,
         )
 
         # If there's no input catalog, select all streams
-        if self._tap_input_catalog is None:
-            self._metadata.root.selected = True
+        self._metadata.root.selected = self._tap_input_catalog is None
 
         return self._metadata
 
