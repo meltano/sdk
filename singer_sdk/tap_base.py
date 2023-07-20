15fd7ad86d09ebbc0486cee1cf332ede2e615d42
Dan Norman <buzzcutnorman@gmail.com>
2023-07-20 11:38:27 -0700

    add _tap_connector type hint

--- singer_sdk/tap_base.py
+++ singer_sdk/tap_base.py
@@ -613,7 +613,7 @@ class SQLTap(Tap):
     # Stream class used to initialize new SQL streams from their catalog declarations.
     default_stream_class: type[SQLStream]
 
-    _tap_connector = None
+    _tap_connector: SQLConnector | None = None
 
     def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
         """Initialize the SQL tap.
