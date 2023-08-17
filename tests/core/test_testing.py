5d8c0be99a195cb9c45647fdfdf1cd73043d93e9
Edgar Ramírez Mondragón <edgar@meltano.com>
2023-08-16 20:07:46 -0600

    Test the test class mro

--- tests/core/test_testing.py
+++ tests/core/test_testing.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 import pytest
 
+from singer_sdk.testing.factory import BaseTestClass
+
 
 def test_module_deprecations():
     with pytest.deprecated_call():
@@ -19,3 +21,23 @@ def test_module_deprecations():
         match="module singer_sdk.testing has no attribute",
     ):
         testing.foo  # noqa: B018
+
+
+def test_test_class_mro():
+    class PluginTestClass(BaseTestClass):
+        pass
+
+    PluginTestClass.params["x"] = 1
+
+    class AnotherPluginTestClass(BaseTestClass):
+        pass
+
+    AnotherPluginTestClass.params["x"] = 2
+    AnotherPluginTestClass.params["y"] = 3
+
+    class SubPluginTestClass(PluginTestClass):
+        pass
+
+    assert PluginTestClass.params == {"x": 1}
+    assert AnotherPluginTestClass.params == {"x": 2, "y": 3}
+    assert SubPluginTestClass.params == {"x": 1}
