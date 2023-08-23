f78cc0a1ed47b2d4d67c08898cebb57b48922fc0
Dan Norman <buzzcutnorman@gmail.com>
2023-08-23 10:58:35 -0700

    added batch_dynamic_management to capabilities

--- singer_sdk/helpers/capabilities.py
+++ singer_sdk/helpers/capabilities.py
@@ -120,6 +120,16 @@ BATCH_WAIT_LIMIT_SECONDS_CONFIG = PropertiesList(
         description="Maximum time to elapse for a batch to fill, drain, and load.",
     ),
 ).to_dict()
+BATCH_DYNAMIC_MANAGEMENT_CONFIG = PropertiesList(
+    Property(
+        "batch_dynamic_management",
+        BooleanType,
+        description=(
+            "Manages sink_max_size per stream to be"
+            "the largest size for the batch wait limit."
+        ),
+    ),
+).to_dict()
 
 
 class DeprecatedEnum(Enum):
