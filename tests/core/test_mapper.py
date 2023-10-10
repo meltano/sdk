d00ba6abbde498ad3edcb595d734c45f802fbbc8
Hal Ali <haleemur@users.noreply.github.com>
2023-10-10 19:07:14 -0400

    feat(taps): add ability to do list comprehensions in stream map expressions (#2003)
    * add ability to do list comprehensions in stream map expressions
    
    * parse expressions only once
    
    * tap factory tests now test with stream map
    
    * remove old vendored file `test_simpleeval.py`
    
    * generate poetry-lock with poetry-v1.6.1
    
    * regenerate lock file
    
    * ignore mypy-3.8 failures

--- tests/core/test_mapper.py
+++ tests/core/test_mapper.py
@@ -22,9 +22,11 @@ from singer_sdk.streams.core import Stream
 from singer_sdk.tap_base import Tap
 from singer_sdk.typing import (
     ArrayType,
+    BooleanType,
     IntegerType,
     NumberType,
     ObjectType,
+    OneOf,
     PropertiesList,
     Property,
     StringType,
@@ -56,6 +58,18 @@ def sample_catalog_dict() -> dict:
         Property("the", StringType),
         Property("brown", StringType),
     ).to_dict()
+    nested_jellybean_schema = PropertiesList(
+        Property("id", IntegerType),
+        Property(
+            "custom_fields",
+            ArrayType(
+                ObjectType(
+                    Property("id", IntegerType),
+                    Property("value", OneOf(StringType, IntegerType, BooleanType)),
+                ),
+            ),
+        ),
+    ).to_dict()
     return {
         "streams": [
             {
@@ -67,6 +81,11 @@ def sample_catalog_dict() -> dict:
                 "stream": "foobars",
                 "tap_stream_id": "foobars",
                 "schema": foobars_schema,
+            },
+            {
+                "stream": "nested_jellybean",
+                "tap_stream_id": "nested_jellybean",
+                "schema": nested_jellybean_schema,
             },
         ],
     }
@@ -110,6 +129,24 @@ def sample_stream():
             {"the": "quick"},
             {"brown": "fox"},
         ],
+        "nested_jellybean": [
+            {
+                "id": 123,
+                "custom_fields": [
+                    {"id": 1, "value": "abc"},
+                    {"id": 2, "value": 1212},
+                    {"id": 3, "value": None},
+                ],
+            },
+            {
+                "id": 124,
+                "custom_fields": [
+                    {"id": 1, "value": "foo"},
+                    {"id": 2, "value": 9009},
+                    {"id": 3, "value": True},
+                ],
+            },
+        ],
     }
 
 
@@ -118,6 +155,19 @@ def sample_stream():
 
 @pytest.fixture
 def transform_stream_maps():
+    nested_jellybean_custom_field_1 = (
+        'dict([(x["id"], x["value"]) for x in custom_fields]).get(1)'
+    )
+    nested_jellybean_custom_field_2 = (
+        'int(dict([(x["id"], x["value"]) for x in custom_fields]).get(2)) '
+        'if dict([(x["id"], x["value"]) for x in custom_fields]).get(2) '
+        "else None"
+    )
+    nested_jellybean_custom_field_3 = (
+        'bool(dict([(x["id"], x["value"]) for x in custom_fields]).get(3)) '
+        'if dict([(x["id"], x["value"]) for x in custom_fields]).get(3) '
+        "else None"
+    )
     return {
         "repositories": {
             "repo_name": "_['name']",
@@ -128,6 +178,12 @@ def transform_stream_maps():
             "create_year": "int(datetime.date.fromisoformat(create_date).year)",
             "int_test": "int('0')",
             "__else__": None,
+        },
+        "nested_jellybean": {
+            "custom_fields": "__NULL__",
+            "custom_field_1": nested_jellybean_custom_field_1,
+            "custom_field_2": nested_jellybean_custom_field_2,
+            "custom_field_3": nested_jellybean_custom_field_3,
         },
     }
 
@@ -185,6 +241,20 @@ def transformed_result(stream_map_config):
             {"the": "quick"},
             {"brown": "fox"},
         ],
+        "nested_jellybean": [
+            {
+                "id": 123,
+                "custom_field_1": "abc",
+                "custom_field_2": 1212,
+                "custom_field_3": None,
+            },
+            {
+                "id": 124,
+                "custom_field_1": "foo",
+                "custom_field_2": 9009,
+                "custom_field_3": True,
+            },
+        ],
     }
 
 
@@ -203,6 +273,12 @@ def transformed_schemas():
         "foobars": PropertiesList(
             Property("the", StringType),
             Property("brown", StringType),
+        ).to_dict(),
+        "nested_jellybean": PropertiesList(
+            Property("id", IntegerType),
+            Property("custom_field_1", StringType),
+            Property("custom_field_2", IntegerType),
+            Property("custom_field_3", BooleanType),
         ).to_dict(),
     }
 
