466d60aee421f7115656d6bb41a73479cce6335f
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2023-11-10 14:02:55 -0600

    docs: Add 404 page with `sphinx-notfound-page` (#2039)

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     "myst_parser",
     "sphinx_reredirects",
     "sphinx_inline_tabs",
+    "notfound.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
