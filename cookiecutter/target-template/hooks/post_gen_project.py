243b399a9a331277fdd45dbdc28ad3350497b033
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2023-11-21 13:07:59 -0600

    Update cookiecutter/target-template/hooks/post_gen_project.py

--- cookiecutter/target-template/hooks/post_gen_project.py
+++ cookiecutter/target-template/hooks/post_gen_project.py
@@ -3,14 +3,12 @@ from pathlib import Path
 import shutil
 
 
-BASE_PATH = Path('{{cookiecutter.library_name}}')
+BASE_PATH = Path("{{cookiecutter.library_name}}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if "{{ cookiecutter.license }}" != "Apache-2.0":
+        Path("LICENSE").unlink()
 
-    if '{{ cookiecutter.license }}' != 'Apache-2.0':
-        Path('LICENSE').unlink()
-
-    if '{{ cookiecutter.include_ci_files }}' != 'GitHub':
-        shutil.rmtree(Path('.github'))
-
+    if "{{ cookiecutter.include_ci_files }}" != "GitHub":
+        shutil.rmtree(Path(".github"))
