ceba9fbb31bd1b561d40aefe2b02ec6f3f109b7d
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-11-21 15:52:14 -0300

    refactor(templates): Simplify target template file names with post_gen_project.py hook

---
+++ cookiecutter/target-template/hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from pathlib import Path
+import shutil
+
+
+BASE_PATH = Path('{{cookiecutter.library_name}}')
+
+
+if __name__ == '__main__':
+
+    if '{{ cookiecutter.license }}' != 'Apache-2.0':
+        Path('LICENSE').unlink()
+
+    if '{{ cookiecutter.include_ci_files }}' != 'GitHub':
+        shutil.rmtree(Path('.github'))
+
