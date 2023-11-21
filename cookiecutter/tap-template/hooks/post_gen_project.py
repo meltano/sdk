ecb79dd8c5afb5a9d667d8ef04d1771d86d28a8b
Edgar Ramírez Mondragón <edgar@meltano.com>
2023-11-21 09:01:38 -0600

    chore: Clean up after #2060

--- cookiecutter/tap-template/hooks/post_gen_project.py
+++ cookiecutter/tap-template/hooks/post_gen_project.py
@@ -3,25 +3,26 @@ from pathlib import Path
 import shutil
 
 
-BASE_PATH = Path('{{cookiecutter.library_name}}')
+PACKAGE_PATH = Path("{{cookiecutter.library_name}}")
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # Rename stream type client and delete others
-    target = Path(BASE_PATH, 'client.py')
-    Path(BASE_PATH, '{{cookiecutter.stream_type|lower}}-client.py').rename(target)
-    [c.unlink() for c in Path(BASE_PATH).rglob("*-client.py")]
+    target = PACKAGE_PATH / "client.py"
+    raw_client_py = PACKAGE_PATH / "{{cookiecutter.stream_type|lower}}-client.py"
+    raw_client_py.rename(target)
 
-    if '{{ cookiecutter.auth_method }}' not in ('OAuth2', 'JWT'):
-        Path(BASE_PATH, 'auth.py').unlink()
+    for client_py in PACKAGE_PATH.rglob("*-client.py"):
+        client_py.unlink()
 
-    if '{{ cookiecutter.stream_type }}' == 'SQL':
-        Path(BASE_PATH, 'streams.py').unlink()
+    if "{{ cookiecutter.auth_method }}" not in ("OAuth2", "JWT"):
+        PACKAGE_PATH.joinpath("auth.py").unlink()
 
-    if '{{ cookiecutter.license }}' != 'Apache-2.0':
-        Path('LICENSE').unlink()
+    if "{{ cookiecutter.stream_type }}" == "SQL":
+        PACKAGE_PATH.joinpath("streams.py").unlink()
 
-    if '{{ cookiecutter.include_ci_files }}' != 'GitHub':
-        shutil.rmtree(Path('.github'))
+    if "{{ cookiecutter.license }}" == "None":
+        Path("LICENSE").unlink()
 
+    if "{{ cookiecutter.include_ci_files }}" != "GitHub":
+        shutil.rmtree(".github")
