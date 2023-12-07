e4846023a7a55d3cf43a3bfece4d391c27655567
Dan Norman <buzzcutnorman@gmail.com>
2023-12-07 14:59:35 -0800

    added enc_hook and dec_hook tests to test_io

--- tests/core/test_io.py
+++ tests/core/test_io.py
@@ -8,7 +8,7 @@ from contextlib import nullcontext
 import msgspec
 import pytest
 
-from singer_sdk.io_base import SingerReader
+from singer_sdk.io_base import SingerReader, dec_hook, enc_hook
 
 
 class DummyReader(SingerReader):
@@ -26,6 +26,26 @@ class DummyReader(SingerReader):
 
     def _process_state_message(self, message_dict: dict) -> None:
         pass
+
+
+def test_dec_hook():
+    test_type = int
+    test_obj = 1
+    expected_value = "1"
+    expceted_type = str
+    returned = dec_hook(type=test_type, obj=test_obj)
+    returned_type = type(returned)
+
+    assert returned == expected_value
+    assert returned_type == expceted_type
+
+
+def test_enc_hook():
+    test_obj = 1
+    expected_value = "1"
+    returned = enc_hook(obj=test_obj)
+
+    assert returned == expected_value
 
 
 @pytest.mark.parametrize(
