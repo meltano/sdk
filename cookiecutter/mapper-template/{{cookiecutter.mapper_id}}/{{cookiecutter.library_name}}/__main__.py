02f628837150537cdba82a2fae1a4d4bd6b992e1
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-01-18 17:31:17 -0600

    chore: Add `__main__.py` entry point to samples and cookiecutters (#2156)
    * chore: Add `__main__.py` entry point to samples and cookiecutters
    
    * Add missing key to replay files

---
+++ cookiecutter/mapper-template/{{cookiecutter.mapper_id}}/{{cookiecutter.library_name}}/__main__.py
@@ -0,0 +1,7 @@
+"""{{ cookiecutter.name }} entry point."""
+
+from __future__ import annotations
+
+from {{ cookiecutter.library_name }}.mapper import {{ cookiecutter.name }}Mapper
+
+{{ cookiecutter.name }}Mapper.cli()
---
+++ samples/sample_tap_sqlite/__main__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from samples.sample_tap_sqlite import SQLiteTap
+
+SQLiteTap.cli()
