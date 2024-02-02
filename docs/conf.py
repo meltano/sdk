5a8702645423af27f88343bad85158e35cfeb1b1
edgarrmondragon <edgarrmondragon@users.noreply.github.com>
2024-02-02 22:08:12 +0000

    chore: Bump package version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2021, Meltano Core Team and Contributors"  # noqa: A001
 author = "Meltano Core Team and Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "0.34.1"
+release = "0.35.0"
 
 
 # -- General configuration ---------------------------------------------------
