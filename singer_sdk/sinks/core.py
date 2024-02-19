678e17ff75868b8cb7b7d0d92e21758c1177fdc8
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-02-19 13:41:36 -0600

    Update singer_sdk/sinks/core.py

--- singer_sdk/sinks/core.py
+++ singer_sdk/sinks/core.py
@@ -289,7 +289,7 @@ class Sink(metaclass=abc.ABCMeta):
             Max number of records to batch before `is_full=True`
 
         .. versionchanged:: 0.36.0
-        This property now takes into account the ``batch_size_rows`` target setting.
+           This property now takes into account the ``batch_size_rows`` target setting.
         """
         return (
             self.batch_size_rows
