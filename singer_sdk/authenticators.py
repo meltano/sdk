c396b6e99184c346b4ed96dd1bc18b5e3e3487c7
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-10 17:25:58 -0600

    Catch exception when jwt deps are missing

--- singer_sdk/authenticators.py
+++ singer_sdk/authenticators.py
@@ -568,11 +568,16 @@ class OAuthJWTAuthenticator(OAuthAuthenticator):
             Payload object for OAuth.
 
         Raises:
+            RuntimeError: If the JWT dependencies are not installed.
             ValueError: If the private key is not set.
         """
-        import jwt  # noqa: PLC0415
-        from cryptography.hazmat.backends import default_backend  # noqa: PLC0415
-        from cryptography.hazmat.primitives import serialization  # noqa: PLC0415
+        try:
+            import jwt  # noqa: PLC0415
+            from cryptography.hazmat.backends import default_backend  # noqa: PLC0415
+            from cryptography.hazmat.primitives import serialization  # noqa: PLC0415
+        except ModuleNotFoundError as ex:
+            msg = "Install singer-sdk[jwt] to use OAuthJWTAuthenticator."
+            raise RuntimeError(msg) from ex
 
         if not self.private_key:
             msg = "Missing 'private_key' property for OAuth payload."
