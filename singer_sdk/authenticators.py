3272f6e9c0bbe38cc48b277bd6306ca338ef456e
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-10 17:39:45 -0600

    Add `pragma: no cover

--- singer_sdk/authenticators.py
+++ singer_sdk/authenticators.py
@@ -575,11 +575,11 @@ class OAuthJWTAuthenticator(OAuthAuthenticator):
             import jwt  # noqa: PLC0415
             from cryptography.hazmat.backends import default_backend  # noqa: PLC0415
             from cryptography.hazmat.primitives import serialization  # noqa: PLC0415
-        except ModuleNotFoundError as ex:
+        except ModuleNotFoundError as ex:  # pragma: no cover
             msg = "Install singer-sdk[jwt] to use OAuthJWTAuthenticator."
             raise RuntimeError(msg) from ex
 
-        if not self.private_key:
+        if not self.private_key:  # pragma: no cover
             msg = "Missing 'private_key' property for OAuth payload."
             raise ValueError(msg)
 
