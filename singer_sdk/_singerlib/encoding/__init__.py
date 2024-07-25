53dd5d7cbadbd1e1202a86b3f4a841bb647c2545
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-07-25 11:22:40 -0400

    chore: Make it easier to switch Singer reader and writer IO implementations in plugin classes

--- singer_sdk/_singerlib/encoding/__init__.py
+++ singer_sdk/_singerlib/encoding/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from ._base import GenericSingerReader, GenericSingerWriter, SingerMessageType
-from ._simple import SingerReader, SingerWriter
+from ._simple import SimpleSingerReader, SimpleSingerWriter
 
 __all__ = [
     "GenericSingerReader",
     "GenericSingerWriter",
+    "SimpleSingerReader",
+    "SimpleSingerWriter",
     "SingerMessageType",
-    "SingerReader",
-    "SingerWriter",
 ]
--- singer_sdk/io_base.py
+++ singer_sdk/io_base.py
@@ -2,4 +2,18 @@
 
 from __future__ import annotations
 
-from singer_sdk._singerlib.encoding import *  # noqa: F403
+from singer_sdk._singerlib.encoding import (
+    GenericSingerReader,
+    GenericSingerWriter,
+    SingerMessageType,
+)
+from singer_sdk._singerlib.encoding import SimpleSingerReader as SingerReader
+from singer_sdk._singerlib.encoding import SimpleSingerWriter as SingerWriter
+
+__all__ = [
+    "GenericSingerReader",
+    "GenericSingerWriter",
+    "SingerMessageType",
+    "SingerReader",
+    "SingerWriter",
+]
