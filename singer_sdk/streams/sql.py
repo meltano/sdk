c3c23512aa374978d65b8d7ad8fcf6b58ccb9e7f
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-08-13 12:59:48 -0600

    refactor: Improved SQL identifier (de)normalization (#2601)
    * refactor: WIP Improved SQL identifier (de)normalization
    
    * Add tests
    
    * Do not use db dialect to generate stream name
    
    * Fix types

--- singer_sdk/streams/sql.py
+++ singer_sdk/streams/sql.py
@@ -14,6 +14,7 @@ from singer_sdk.connectors import SQLConnector
 from singer_sdk.streams.core import REPLICATION_INCREMENTAL, Stream
 
 if t.TYPE_CHECKING:
+    from singer_sdk.connectors.sql import FullyQualifiedName
     from singer_sdk.helpers.types import Context
     from singer_sdk.tap_base import Tap
 
@@ -124,7 +125,7 @@ class SQLStream(Stream, metaclass=abc.ABCMeta):
         self._singer_catalog_entry.metadata.root.table_key_properties = new_value
 
     @property
-    def fully_qualified_name(self) -> str:
+    def fully_qualified_name(self) -> FullyQualifiedName:
         """Generate the fully qualified version of the table name.
 
         Raises:
