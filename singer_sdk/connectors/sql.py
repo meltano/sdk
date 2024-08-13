92d5156e6db1943f5411e3e65faa0ddbdd6c665b
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-08-12 20:18:09 -0600

    refactor: WIP Improved SQL identifier (de)normalization

--- singer_sdk/connectors/sql.py
+++ singer_sdk/connectors/sql.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import logging
 import typing as t
 import warnings
+from collections import UserString
 from contextlib import contextmanager
 from datetime import datetime
 from functools import lru_cache
@@ -20,6 +21,89 @@ from singer_sdk.helpers.capabilities import TargetLoadMethods
 if t.TYPE_CHECKING:
     from sqlalchemy.engine import Engine
     from sqlalchemy.engine.reflection import Inspector
+
+
+class FullyQualifiedName(UserString):
+    """A fully qualified table name.
+
+    This class provides a simple way to represent a fully qualified table name
+    as a single object. The string representation of this object is the fully
+    qualified table name, with the parts separated by periods.
+
+    The parts of the fully qualified table name are:
+    - database
+    - schema
+    - table
+
+    The database and schema are optional. If only the table name is provided,
+    the string representation of the object will be the table name alone.
+
+    Example:
+    ```
+    table_name = FullyQualifiedName("my_table", "my_schema", "my_db")
+    print(table_name)  # my_db.my_schema.my_table
+    ```
+    """
+
+    def __init__(
+        self,
+        *,
+        table: str | None = None,
+        schema: str | None = None,
+        database: str | None = None,
+        delimiter: str = ".",
+        dialect: sa.engine.Dialect,
+    ) -> None:
+        """Initialize the fully qualified table name.
+
+        Args:
+            table: The name of the table.
+            schema: The name of the schema. Defaults to None.
+            database: The name of the database. Defaults to None.
+            delimiter: The delimiter to use between parts. Defaults to '.'.
+            dialect: The SQLAlchemy dialect to use for quoting.
+
+        Raises:
+            ValueError: If the fully qualified name could not be generated.
+        """
+        self.table = table
+        self.schema = schema
+        self.database = database
+        self.delimiter = delimiter
+        self.dialect = dialect
+
+        parts = []
+        if self.database:
+            parts.append(self.prepare_part(self.database))
+        if self.schema:
+            parts.append(self.prepare_part(self.schema))
+        if self.table:
+            parts.append(self.prepare_part(self.table))
+
+        if not parts:
+            raise ValueError(
+                "Could not generate fully qualified name: "
+                + ":".join(
+                    [
+                        self.database or "(unknown-db)",
+                        self.schema or "(unknown-schema)",
+                        self.table or "(unknown-table-name)",
+                    ],
+                ),
+            )
+
+        super().__init__(self.delimiter.join(parts))
+
+    def prepare_part(self, part: str) -> str:
+        """Prepare a part of the fully qualified name.
+
+        Args:
+            part: The part to prepare.
+
+        Returns:
+            The prepared part.
+        """
+        return self.dialect.identifier_preparer.quote(part)
 
 
 class SQLConnector:  # noqa: PLR0904
@@ -238,13 +322,13 @@ class SQLConnector:  # noqa: PLR0904
         """
         return th.to_sql_type(jsonschema_type)
 
-    @staticmethod
     def get_fully_qualified_name(
+        self,
         table_name: str | None = None,
         schema_name: str | None = None,
         db_name: str | None = None,
         delimiter: str = ".",
-    ) -> str:
+    ) -> FullyQualifiedName:
         """Concatenates a fully qualified name from the parts.
 
         Args:
@@ -253,34 +337,16 @@ class SQLConnector:  # noqa: PLR0904
             db_name: The name of the database. Defaults to None.
             delimiter: Generally: '.' for SQL names and '-' for Singer names.
 
-        Raises:
-            ValueError: If all 3 name parts not supplied.
-
         Returns:
             The fully qualified name as a string.
         """
-        parts = []
-
-        if db_name:
-            parts.append(db_name)
-        if schema_name:
-            parts.append(schema_name)
-        if table_name:
-            parts.append(table_name)
-
-        if not parts:
-            raise ValueError(
-                "Could not generate fully qualified name: "
-                + ":".join(
-                    [
-                        db_name or "(unknown-db)",
-                        schema_name or "(unknown-schema)",
-                        table_name or "(unknown-table-name)",
-                    ],
-                ),
-            )
-
-        return delimiter.join(parts)
+        return FullyQualifiedName(
+            table=table_name,
+            schema=schema_name,
+            database=db_name,
+            delimiter=delimiter,
+            dialect=self._dialect,
+        )
 
     @property
     def _dialect(self) -> sa.engine.Dialect:
@@ -429,11 +495,14 @@ class SQLConnector:  # noqa: PLR0904
             `CatalogEntry` object for the given table or a view
         """
         # Initialize unique stream name
-        unique_stream_id = self.get_fully_qualified_name(
-            db_name=None,
-            schema_name=schema_name,
-            table_name=table_name,
-            delimiter="-",
+        # We cast UserString to str to make s.join() happy
+        unique_stream_id = str(
+            self.get_fully_qualified_name(
+                db_name=None,
+                schema_name=schema_name,
+                table_name=table_name,
+                delimiter="-",
+            )
         )
 
         # Detect key properties
@@ -528,7 +597,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def parse_full_table_name(  # noqa: PLR6301
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
     ) -> tuple[str | None, str | None, str]:
         """Parse a fully qualified table name into its parts.
 
@@ -547,6 +616,13 @@ class SQLConnector:  # noqa: PLR0904
             A three part tuple (db_name, schema_name, table_name) with any unspecified
             or unused parts returned as None.
         """
+        if isinstance(full_table_name, FullyQualifiedName):
+            return (
+                full_table_name.database,
+                full_table_name.schema,
+                full_table_name.table,
+            )
+
         db_name: str | None = None
         schema_name: str | None = None
 
@@ -587,7 +663,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def get_table_columns(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_names: list[str] | None = None,
     ) -> dict[str, sa.Column]:
         """Return a list of table columns.
@@ -618,7 +694,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def get_table(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_names: list[str] | None = None,
     ) -> sa.Table:
         """Return a table object.
@@ -643,7 +719,9 @@ class SQLConnector:  # noqa: PLR0904
             schema=schema_name,
         )
 
-    def column_exists(self, full_table_name: str, column_name: str) -> bool:
+    def column_exists(
+        self, full_table_name: str | FullyQualifiedName, column_name: str
+    ) -> bool:
         """Determine if the target table already exists.
 
         Args:
@@ -666,7 +744,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def create_empty_table(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         schema: dict,
         primary_keys: t.Sequence[str] | None = None,
         partition_keys: list[str] | None = None,
@@ -715,7 +793,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def _create_empty_column(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_name: str,
         sql_type: sa.types.TypeEngine,
     ) -> None:
@@ -753,7 +831,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def prepare_table(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         schema: dict,
         primary_keys: t.Sequence[str],
         partition_keys: list[str] | None = None,
@@ -797,7 +875,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def prepare_column(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_name: str,
         sql_type: sa.types.TypeEngine,
     ) -> None:
@@ -822,7 +900,9 @@ class SQLConnector:  # noqa: PLR0904
             sql_type=sql_type,
         )
 
-    def rename_column(self, full_table_name: str, old_name: str, new_name: str) -> None:
+    def rename_column(
+        self, full_table_name: str | FullyQualifiedName, old_name: str, new_name: str
+    ) -> None:
         """Rename the provided columns.
 
         Args:
@@ -951,7 +1031,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def _get_column_type(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_name: str,
     ) -> sa.types.TypeEngine:
         """Get the SQL type of the declared column.
@@ -1096,7 +1176,7 @@ class SQLConnector:  # noqa: PLR0904
 
     def _adapt_column_type(
         self,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         column_name: str,
         sql_type: sa.types.TypeEngine,
     ) -> None:
@@ -1187,7 +1267,7 @@ class SQLConnector:  # noqa: PLR0904
     def delete_old_versions(
         self,
         *,
-        full_table_name: str,
+        full_table_name: str | FullyQualifiedName,
         version_column_name: str,
         current_version: int,
     ) -> None:
