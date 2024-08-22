5213d08503ce022e5f04362a184480a586a8b519
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-08-21 20:22:07 -0600

    feat: Interruption and termination signals in taps and targets

--- singer_sdk/tap_base.py
+++ singer_sdk/tap_base.py
@@ -35,6 +35,7 @@ from singer_sdk.singerlib import Catalog, StateMessage
 
 if t.TYPE_CHECKING:
     from pathlib import PurePath
+    from types import FrameType
 
     from singer_sdk.connectors import SQLConnector
     from singer_sdk.mapper import PluginMapper
@@ -497,6 +498,22 @@ class Tap(BaseSingerWriter, metaclass=abc.ABCMeta):  # noqa: PLR0904
             stream.log_sync_costs()
 
     # Command Line Execution
+
+    def _handle_termination(  # pragma: no cover
+        self,
+        signum: int,
+        frame: FrameType | None,
+    ) -> None:
+        """Handle termination signal.
+
+        Args:
+            signum: Signal number.
+            frame: Frame.
+        """
+        # Emit a final state message to ensure the state is written to the output
+        # even if the process is terminated by a signal.
+        self.write_message(StateMessage(value=self.state))
+        super()._handle_termination(signum, frame)
 
     @classmethod
     def invoke(  # type: ignore[override]
