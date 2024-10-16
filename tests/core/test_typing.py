fbf60a17abaf9e62c81b588011abdb47cd88d193
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-10-16 14:58:27 -0600

    Test one more case

--- tests/core/test_typing.py
+++ tests/core/test_typing.py
@@ -313,6 +313,12 @@ def test_conform_primitives():
 
     assert _conform_primitive_property(None, {"type": "boolean"}) is None
     assert _conform_primitive_property(0, {"type": "boolean"}) is False
+    assert (
+        _conform_primitive_property(
+            0, {"anyOf": [{"type": "boolean"}, {"type": "integer"}]}
+        )
+        == 0
+    )
     assert _conform_primitive_property(0, {"type": ["boolean", "integer"]}) == 0
     assert _conform_primitive_property(1, {"type": "boolean"}) is True
     assert _conform_primitive_property(1, {"type": ["boolean", "null"]}) is True
