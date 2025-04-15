72939f2a371296681dff595ab9ba32a5dfa1b007
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-04-15 14:12:32 -0600

    fix: Do not log a warning if `null` cannot be appended to schema types (#2974)
    * fix: Do not log a warning if `null` cannot be appended to schema types
    
    * Update in docs and templates
    
    * Update docs/incremental_replication.md
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cookiecutter/target-template/{{cookiecutter.target_id}}/{{cookiecutter.library_name}}/target.py
+++ cookiecutter/target-template/{{cookiecutter.target_id}}/{{cookiecutter.library_name}}/target.py
@@ -21,7 +21,7 @@ class Target{{ cookiecutter.destination_name }}({{ target_class }}):
         {%- if cookiecutter.serialization_method == "SQL" %}
         th.Property(
             "sqlalchemy_url",
-            th.StringType,
+            th.StringType(nullable=False),
             secret=True,  # Flag config as protected.
             title="SQLAlchemy URL",
             description="SQLAlchemy connection string",
@@ -29,20 +29,21 @@ class Target{{ cookiecutter.destination_name }}({{ target_class }}):
         {%- else %}
         th.Property(
             "filepath",
-            th.StringType,
+            th.StringType(nullable=False),
             title="Output File Path",
             description="The path to the target output file",
         ),
         th.Property(
             "file_naming_scheme",
-            th.StringType,
+            th.StringType(nullable=False),
             title="File Naming Scheme",
             description="The scheme with which output files will be named",
         ),
         th.Property(
             "auth_token",
-            th.StringType,
+            th.StringType(nullable=False),
             secret=True,  # Flag config as protected.
+            required=True,
             title="Auth Token",
             description="The path to the target output file",
         ),
