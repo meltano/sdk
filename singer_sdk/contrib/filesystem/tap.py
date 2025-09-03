c862d8d132f52afd765902d55fe0d50ab175a1c9
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-09-03 13:21:04 -0600

    chore: Remove unnecessary type arg

--- singer_sdk/contrib/filesystem/tap.py
+++ singer_sdk/contrib/filesystem/tap.py
@@ -110,7 +110,7 @@ class FolderTap(Tap, t.Generic[_T]):
     config_jsonschema: t.ClassVar[dict] = {"properties": {}}
 
     @classmethod
-    def append_builtin_config(cls: type[FolderTap], config_jsonschema: dict) -> None:
+    def append_builtin_config(cls, config_jsonschema: dict) -> None:
         """Appends built-in config to `config_jsonschema` if not already set.
 
         To customize or disable this behavior, developers may either override this class
